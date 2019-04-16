0ab0d8c25cbba197dd2349833b9c430d45fda741
spamviech <spaviech@web.de>
2019-04-16 22:46:37 +0200

    Durch ziehen einer .json-Datei auf die Executable wird versucht diese bei Programmstart zu laden.

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,16 @@
 {-# LANGUAGE NamedFieldPuns #-}
 
+import System.Environment (getArgs, withArgs)
 import qualified Zug.UI as UI
 
 main :: IO ()
-main = UI.main
+main = do
+    -- Wenn nur ein Kommandozeilenargument übergeben wurde, welches nicht mit '-' beginnt, versuche es als Datei zu laden.
+    -- Damit kann das Programm durch ziehen einer .json-Datei auf die Executable mit einem bestimmten Anfangszustand geladen werden.
+    args <- getArgs
+    argModifier args UI.main
+        where
+            argModifier :: [String] -> IO a -> IO a
+            argModifier (('-':_arg):([]))   = id
+            argModifier (arg:[])            = withArgs ["--load=" ++ arg]
+            argModifier _args               = id
