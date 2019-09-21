bb9e31c64e90ee6bb91404b9ff9b10d2d6b4542b
spamviech <spaviech@web.de>
2019-09-21 18:35:49 +0200

    Befehl.hs: Kinds korrigiert

--- src/Zug/UI/Befehl.hs
+++ src/Zug/UI/Befehl.hs
@@ -3,6 +3,7 @@
 {-# LANGUAGE InstanceSigs #-}
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE DataKinds #-}
 
 {-|
 Description : Alle durch ein UI unterstützten Befehle, inklusive der Implementierung.
@@ -25,12 +26,13 @@ import Data.Aeson (ToJSON)
 import Numeric.Natural (Natural)
 -- Abhängigkeiten von anderen Modulen
 import Zug.Anbindung (PwmMap, pwmMapEmpty, i2cMapEmpty)
+import Zug.Klassen (Zugtyp(..))
 import Zug.Menge (Menge, entfernen)
 import Zug.Plan (ObjektKlasse(..), ObjektAllgemein(..), Objekt, PlanKlasse(), Plan(),
                 Ausführend(..), AktionKlasse(), Aktion(), ausführenPlan, ausführenAktion)
 import qualified Zug.UI.Save as Save
 import Zug.UI.Base (StatusAllgemein(), Status, IOStatusAllgemein,
-                    auswertenTMVarIOStatus, übergebeTVarPwmMap, liftIOFunction,
+                    auswertenTMVarIOStatus, übergebeTVarMaps, liftIOFunction,
                     getTVarAusführend, getTVarPwmMap, getTVarI2CMap,
                     hinzufügenPlan, entfernenPlan,
                     hinzufügenWegstrecke, entfernenWegstrecke,
@@ -58,51 +60,72 @@ ausführenTMVarAktion   :: (AktionKlasse a)
 ausführenTMVarAktion aktion tmvarStatus = auswertenTMVarIOStatus getTVarPwmMap tmvarStatus >>= ausführenAktion aktion
 
 -- | Führe einen Befehl mit einem in einer MVar gespeichertem Zustand aus
-ausführenTMVarBefehl :: (BefehlKlasse b, ObjektKlasse o, ToJSON o, Eq (BG o) , Eq (ST o) , Eq (WE o) , Eq (KU o) , Eq (WS o) , Eq (PL o))
-                    => b o -> TMVar (StatusAllgemein o) -> IO Bool
+ausführenTMVarBefehl :: (BefehlKlasse b, ObjektKlasse o, ToJSON o, Eq ((BG o) 'Märklin), Eq ((BG o) 'Lego),
+                        Eq (ST o), Eq ((WE o) 'Märklin), Eq ((WE o) 'Lego), Eq (KU o),
+                        Eq ((WS o) 'Märklin), Eq ((WS o) 'Lego), Eq (PL o))
+                            => b o -> TMVar (StatusAllgemein o) -> IO Bool
 ausführenTMVarBefehl befehl = auswertenTMVarIOStatus $ ausführenBefehl befehl
 
 -- | Ausführen eines Befehls
 class BefehlKlasse b where
     -- | Gibt True zurück, falls das UI beendet werden soll
-    ausführenBefehl :: (ObjektKlasse o, ToJSON o, Eq (BG o) , Eq (ST o) , Eq (WE o) , Eq (KU o) , Eq (WS o) , Eq (PL o))
-                    => b o -> IOStatusAllgemein o Bool
+    ausführenBefehl :: (ObjektKlasse o, ToJSON o, Eq ((BG o) 'Märklin), Eq ((BG o) 'Lego), Eq (ST o), Eq (PL o),
+                        Eq ((WE o) 'Märklin), Eq ((WE o) 'Lego), Eq (KU o), Eq ((WS o) 'Märklin), Eq ((WS o) 'Lego))
+                            => b o -> IOStatusAllgemein o Bool
 
 -- | Unterstütze Befehle
-data BefehlAllgemein o  = UI                    (UIBefehlAllgemein o)
-                        | Hinzufügen            o
-                        | Entfernen             o
-                        | Speichern             FilePath
-                        | Laden                 FilePath                (Status -> IO (StatusAllgemein o))  (IOStatusAllgemein o ())
-                        | Ausführen             Plan                    (Natural -> IO ())                  (IO ())
-                        | AusführenAbbrechen    Plan
-                        | AktionBefehl          Aktion
--- | 'BefehlAllgemein' spezialisiert auf minimal spezialisierte Typen
+data BefehlAllgemein o
+    = UI
+        (UIBefehlAllgemein o)
+    | Hinzufügen
+        o
+    | Entfernen
+        o
+    | Speichern
+        FilePath
+    | Laden
+        FilePath
+        (Status -> IO (StatusAllgemein o))  -- ^ Erfolgsaktion
+        (IOStatusAllgemein o ())            -- ^ Fehlerbehandlung
+    | Ausführen
+        Plan
+        (Natural -> IO ())                  -- ^ Fortschrittsanzeige
+        (IO ())                             -- ^ Abschlussanzeige
+    | AusführenAbbrechen
+        Plan
+    | AktionBefehl
+        Aktion
+-- | 'BefehlAllgemein' spezialisiert auf 'Objekt'
 type Befehl = BefehlAllgemein Objekt
 
 -- | UI-spezifische Befehle. Phantomtyp, um eine 'BefehlKlasse'-Instanz zu erhalten.
 data UIBefehlAllgemein o = Beenden | Abbrechen
                             deriving (Show)
--- | 'UIBefehlAllgemein' spezialisiert auf minimal benötigte Typen
+-- | 'UIBefehlAllgemein' spezialisiert auf 'Objekt'
 type UIBefehl = UIBefehlAllgemein Objekt
 
 instance BefehlKlasse UIBefehlAllgemein where
-    ausführenBefehl :: (ObjektKlasse o, ToJSON o, Eq (BG o) , Eq (ST o) , Eq (WE o) , Eq (KU o) , Eq (WS o) , Eq (PL o))
-                    => UIBefehlAllgemein o -> IOStatusAllgemein o Bool
+    ausführenBefehl :: (ObjektKlasse o, ToJSON o, Eq ((BG o) 'Märklin), Eq ((BG o) 'Lego), Eq (ST o), Eq (PL o),
+                        Eq ((WE o) 'Märklin), Eq ((WE o) 'Lego), Eq (KU o), Eq ((WS o) 'Märklin), Eq ((WS o) 'Lego))
+                            => UIBefehlAllgemein o -> IOStatusAllgemein o Bool
     ausführenBefehl Beenden     = pure True
     ausführenBefehl Abbrechen   = pure False
 
 instance BefehlKlasse BefehlAllgemein where
-    ausführenBefehl :: (ObjektKlasse o, ToJSON o, Eq (BG o) , Eq (ST o) , Eq (WE o) , Eq (KU o) , Eq (WS o) , Eq (PL o))
-                    => BefehlAllgemein o -> IOStatusAllgemein o Bool
-    ausführenBefehl (UI Beenden)    = pure True
-    ausführenBefehl (UI Abbrechen)  = pure False
-    ausführenBefehl befehl          = ausführenBefehlAux befehl >> pure False
+    ausführenBefehl :: (ObjektKlasse o, ToJSON o, Eq ((BG o) 'Märklin), Eq ((BG o) 'Lego), Eq (ST o), Eq (PL o),
+                        Eq ((WE o) 'Märklin), Eq ((WE o) 'Lego), Eq (KU o), Eq ((WS o) 'Märklin), Eq ((WS o) 'Lego))
+                            => BefehlAllgemein o -> IOStatusAllgemein o Bool
+    ausführenBefehl befehl = ausführenBefehlAux befehl >> pure (istBeenden befehl)
         where
-            ausführenBefehlAux :: (ObjektKlasse o, ToJSON o, Eq (BG o) , Eq (ST o) , Eq (WE o) , Eq (KU o) , Eq (WS o) , Eq (PL o))
-                                => BefehlAllgemein o -> IOStatusAllgemein o ()
+            istBeenden :: BefehlAllgemein o -> Bool
+            istBeenden  (UI Beenden)    = True
+            istBeenden  _befehl         = False
+            ausführenBefehlAux :: (ObjektKlasse o, ToJSON o, Eq ((BG o) 'Märklin), Eq ((BG o) 'Lego), Eq (ST o),
+                                    Eq ((WE o) 'Märklin), Eq ((WE o) 'Lego), Eq (KU o),
+                                    Eq ((WS o) 'Märklin), Eq ((WS o) 'Lego), Eq (PL o))
+                                        => BefehlAllgemein o -> IOStatusAllgemein o ()
             ausführenBefehlAux  (UI uiAction)
-                = error $ "Vergessene UI-Aktion: " ++ show uiAction
+                = pure ()
             ausführenBefehlAux  (Hinzufügen objekt)
                 = case erhalteObjekt objekt of
                     (OPlan plan)                                -> hinzufügenPlan plan
@@ -135,26 +158,33 @@ instance BefehlKlasse BefehlAllgemein where
             ausführenBefehlAux  (Ausführen plan showAction endAktion)
                 = do
                     tvarAusführend <- getTVarAusführend
-                    übergebeTVarPwmMap $ ausführenPlan plan showAction endAktion tvarAusführend
+                    übergebeTVarMaps $ ausführenPlan plan showAction endAktion tvarAusführend
             ausführenBefehlAux  (AusführenAbbrechen plan)
                 = do
                     tvarAusführend <- getTVarAusführend
                     liftIO $ atomically $ modifyTVar tvarAusführend $ entfernen $ Ausführend plan
             ausführenBefehlAux  (AktionBefehl aktion)
-                = übergebeTVarPwmMap $ ausführenAktion aktion
+                = übergebeTVarMaps $ ausführenAktion aktion
 
--- | Normale Listen von 'BefehlAllgemein' haben den falschen Kind um eine 'BefehlKlasse'-Instanz zu erhalten. Daher wird ein newtype benötigt.
+-- | Normale Listen von 'BefehlAllgemein' haben den falschen Kind um eine 'BefehlKlasse'-Instanz zu erhalten.
+-- Der hier bereitgestellte ein newtype löst das Problem über einen Phantomtyp.
 newtype BefehlListeAllgemein o = BefehlListe {getBefehlListe :: [BefehlAllgemein o]}
--- | 'BefehlListeAllgemein' spezialisiert auf minimal benötigte Typen
+-- | 'BefehlListeAllgemein' spezialisiert auf 'Objekt'
 type BefehlListe = BefehlListeAllgemein Objekt
 
 instance BefehlKlasse BefehlListeAllgemein where
-    ausführenBefehl :: (ObjektKlasse o, ToJSON o, Eq (BG o) , Eq (ST o) , Eq (WE o) , Eq (KU o) , Eq (WS o) , Eq (PL o))
-                    => BefehlListeAllgemein o -> IOStatusAllgemein o Bool
+    ausführenBefehl :: (ObjektKlasse o, ToJSON o, Eq ((BG o) 'Märklin), Eq ((BG o) 'Lego), Eq (ST o), Eq (PL o),
+                        Eq ((WE o) 'Märklin), Eq ((WE o) 'Lego), Eq (KU o), Eq ((WS o) 'Märklin), Eq ((WS o) 'Lego))
+                            => BefehlListeAllgemein o -> IOStatusAllgemein o Bool
     ausführenBefehl (BefehlListe liste) = ausführenBefehlAux liste
         where
-            ausführenBefehlAux  :: (ObjektKlasse o, ToJSON o , Eq (BG o) , Eq (ST o) , Eq (WE o) , Eq (KU o) , Eq (WS o) , Eq (PL o))
-                                =>  [BefehlAllgemein o] -> IOStatusAllgemein o Bool
+            ausführenBefehlAux  :: (ObjektKlasse o, ToJSON o, Eq ((BG o) 'Märklin), Eq ((BG o) 'Lego), Eq (ST o),
+                                    Eq ((WE o) 'Märklin), Eq ((WE o) 'Lego), Eq (KU o),
+                                    Eq ((WS o) 'Märklin), Eq ((WS o) 'Lego), Eq (PL o))
+                                        =>  [BefehlAllgemein o] -> IOStatusAllgemein o Bool
             ausführenBefehlAux []      = pure False
-            ausführenBefehlAux (h:[])  = ausführenBefehl h
-            ausführenBefehlAux (h:t)   = ausführenBefehl h >>= \ende -> if ende then pure True else ausführenBefehlAux t
+            ausführenBefehlAux (h:t)   = do
+                ende <- ausführenBefehl h
+                if ende
+                    then pure True
+                    else ausführenBefehlAux t
