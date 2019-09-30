e285d75d0b0906995c461c04500b9ceefc0c2b3b
spamviech <spamviech@web.de>
2019-09-30 02:59:04 +0200

    ScrollbaresWidget done

--- src/Zug/UI/GTK/Widget/ScrollbaresWidget.hs
+++ src/Zug/UI/GTK/Widget/ScrollbaresWidget.hs
@@ -1,4 +1,6 @@
+{-# LANGUAGE InstanceSigs #-}
 {-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE MonoLocalBinds #-}
 {-# LANGUAGE CPP #-}
 
 {-|
@@ -11,9 +13,13 @@ module Zug.UI.Gtk.Widget.ScrollbaresWidget (
     ScrollbaresWidget(), scrollbaresWidgetNew, scrollbaresWidgetAddNew,
     scrollbaresWidgetPackNew, scrollbaresWidgetNotebookAppendPageNew) where
 
+import Control.Monad.Trans (MonadIO(..))
 import Data.Text (Text)
+import Graphics.UI.Gtk (AttrOp(..))
 import qualified Graphics.UI.Gtk as Gtk
 -- Abhängigkeit von anderen Modulen
+import Zug.UI.Gtk.Widget.Hilfsfunktionen (widgetShowNew, containerAddWidgetNew,
+                                        boxPackWidgetNew, Packing(..), paddingDefault, positionDefault, notebookAppendPageNew)
 import Zug.UI.Gtk.Widget.Klassen (MitWidget(..),  MitContainer(..), MitBox(..), MitGrid(..), MitFixed(..), MitLabel(..),
                                     MitNotebook(..), MitPaned(..), MitComboBox(..), MitWindow(..), MitDialog(..),
                                     MitButton(..), MitToggleButton(..), MitCheckButton(..), MitRadioButton(..))
@@ -21,81 +27,76 @@ import Zug.UI.Gtk.Widget.Klassen (MitWidget(..),  MitContainer(..), MitBox(..), 
 -- | ScrolledWindow mit automatisch erstelltem Viewport
 data ScrollbaresWidget w = ScrollbaresWidget {swScrolledWindow :: Gtk.ScrolledWindow, swWidget :: w}
 
-instance MitWidget (ScrolledWidget w) where
-    erhalteWidget :: ScrolledWidget w -> Gtk.Widget
+instance MitWidget (ScrollbaresWidget w) where
+    erhalteWidget :: ScrollbaresWidget w -> Gtk.Widget
     erhalteWidget = erhalteWidget . swScrolledWindow
 
 instance (MitContainer w) => MitContainer (ScrollbaresWidget w) where
-    erhalteContainer :: ScrollbaresWidget w -> Gtk.
+    erhalteContainer :: ScrollbaresWidget w -> Gtk.Container
     erhalteContainer = erhalteContainer . swWidget
 instance (MitBox w) => MitBox (ScrollbaresWidget w) where
-    erhalteBox :: ScrollbaresWidget w -> Gtk.
+    erhalteBox :: ScrollbaresWidget w -> Gtk.Box
     erhalteBox = erhalteBox . swWidget
-instance (Mit w) => MitGrid (ScrollbaresWidget w) where
-    erhalteGrid :: ScrollbaresWidget w -> Gtk.
+instance (MitGrid w) => MitGrid (ScrollbaresWidget w) where
+    erhalteGrid :: ScrollbaresWidget w -> Gtk.Grid
     erhalteGrid = erhalteGrid . swWidget
 instance (MitFixed w) => MitFixed (ScrollbaresWidget w) where
-    erhalteFixed :: ScrollbaresWidget w -> Gtk.
+    erhalteFixed :: ScrollbaresWidget w -> Gtk.Fixed
     erhalteFixed = erhalteFixed . swWidget
 instance (MitLabel w) => MitLabel (ScrollbaresWidget w) where
-    erhalteLabel :: ScrollbaresWidget w -> Gtk.
+    erhalteLabel :: ScrollbaresWidget w -> Gtk.Label
     erhalteLabel = erhalteLabel . swWidget
 instance (MitNotebook w) => MitNotebook (ScrollbaresWidget w) where
-    erhalteNotebook :: ScrollbaresWidget w -> Gtk.
+    erhalteNotebook :: ScrollbaresWidget w -> Gtk.Notebook
     erhalteNotebook = erhalteNotebook . swWidget
-instance (Mit w) => MitPaned (ScrollbaresWidget w) where
-    erhaltePaned :: ScrollbaresWidget w -> Gtk.
+instance (MitPaned w) => MitPaned (ScrollbaresWidget w) where
+    erhaltePaned :: ScrollbaresWidget w -> Gtk.Paned
     erhaltePaned = erhaltePaned . swWidget
 instance (MitComboBox w) => MitComboBox (ScrollbaresWidget w) where
-    erhalteComboBox :: ScrollbaresWidget w -> Gtk.
+    erhalteComboBox :: ScrollbaresWidget w -> Gtk.ComboBox
     erhalteComboBox = erhalteComboBox . swWidget
 instance (MitWindow w) => MitWindow (ScrollbaresWidget w) where
-    erhalteWindow :: ScrollbaresWidget w -> Gtk.
+    erhalteWindow :: ScrollbaresWidget w -> Gtk.Window
     erhalteWindow = erhalteWindow . swWidget
 instance (MitDialog w) => MitDialog (ScrollbaresWidget w) where
-    erhalteDialog :: ScrollbaresWidget w -> Gtk.
+    erhalteDialog :: ScrollbaresWidget w -> Gtk.Dialog
     erhalteDialog = erhalteDialog . swWidget
 instance (MitButton w) => MitButton (ScrollbaresWidget w) where
-    erhalteButton :: ScrollbaresWidget w -> Gtk.
+    erhalteButton :: ScrollbaresWidget w -> Gtk.Button
     erhalteButton = erhalteButton . swWidget
 instance (MitToggleButton w) => MitToggleButton (ScrollbaresWidget w) where
-    erhalteToggleButton :: ScrollbaresWidget w -> Gtk.
+    erhalteToggleButton :: ScrollbaresWidget w -> Gtk.ToggleButton
     erhalteToggleButton = erhalteToggleButton . swWidget
 instance (MitCheckButton w) => MitCheckButton (ScrollbaresWidget w) where
-    erhalteCheckButton :: ScrollbaresWidget w -> Gtk.
+    erhalteCheckButton :: ScrollbaresWidget w -> Gtk.CheckButton
     erhalteCheckButton = erhalteCheckButton . swWidget
 instance (MitRadioButton w) => MitRadioButton (ScrollbaresWidget w) where
-    erhalteRadioButton :: ScrollbaresWidget w -> Gtk.
+    erhalteRadioButton :: ScrollbaresWidget w -> Gtk.RadioButton
     erhalteRadioButton = erhalteRadioButton . swWidget
 
 -- | Erstelle neues 'Scrollbares
-scrollbaresWidgetNew :: (MitWidget w) => IO w -> IO (Gtk.ScrolledWindow, w)
+scrollbaresWidgetNew :: (MonadIO m, MitWidget w) => m w -> m (ScrollbaresWidget w)
 scrollbaresWidgetNew konstruktor = do
     widget <- widgetShowNew konstruktor
-    scrolledWindow <- widgetShowNew $ scrolledWindowNew Nothing Nothing
-    set scrolledWindow [scrolledWindowHscrollbarPolicy := PolicyNever, scrolledWindowVscrollbarPolicy := PolicyAlways]
-    scrolledWindowAddWithViewport scrolledWindow widget
-    pure (scrolledWindow, widget)
+    liftIO $ do
+        swScrolledWindow <- widgetShowNew $ Gtk.scrolledWindowNew Nothing Nothing
+        Gtk.set swScrolledWindow [
+            Gtk.scrolledWindowHscrollbarPolicy := Gtk.PolicyNever,
+            Gtk.scrolledWindowVscrollbarPolicy := Gtk.PolicyAlways]
+        Gtk.scrolledWindowAddWithViewport swScrolledWindow $ erhalteWidget widget
+        pure ScrollbaresWidget {swScrolledWindow, swWidget = widget}
 
 -- | Erstelle neues 'ScrolledWindow' mit automatisch erstelltem Viewport und füge sie zu 'Container' hinzu
-scrollbaresWidgetAddNew :: (MitContainer c, MitWidget w) => c -> IO w -> IO (ScrolledWindow, w)
-scrollbaresWidgetAddNew container konstruktor = do
-    (scrolledWindow, widget) <- scrolledWidgetNew konstruktor
-    containerAddWidgetNew container $ pure scrolledWindow
-    pure (scrolledWindow, widget)
+scrollbaresWidgetAddNew :: (MonadIO m, MitContainer c, MitWidget w) => c -> m w -> m (ScrollbaresWidget w)
+scrollbaresWidgetAddNew container konstruktor =containerAddWidgetNew container $ scrollbaresWidgetNew konstruktor
 
 -- | Erstelle neues 'ScrolledWindow' mit automatisch erstelltem Viewport und packe sie in eine 'Box'
-scrollbaresWidgetPackNew :: (MitBox b, MitWidget w) => b -> IO w -> IO (ScrolledWindow, w)
-scrollbaresWidgetPackNew box konstruktor = do
-    (scrolledWindow, widget) <- scrolledWidgetNew konstruktor
-    boxPackWidgetNew box PackGrow paddingDefault positionDefault $ pure scrolledWindow
-    pure (scrolledWindow, widget)
+scrollbaresWidgetPackNew :: (MonadIO m, MitBox b, MitWidget w) => b -> m w -> m (ScrollbaresWidget w)
+scrollbaresWidgetPackNew box konstruktor
+    = boxPackWidgetNew box PackGrow paddingDefault positionDefault $ scrollbaresWidgetNew konstruktor
 
 -- | Seite mit scrollbarer VBox einem Notebook hinzufügen
-scrollbaresWidgetNotebookAppendPageNew :: (MitNotebook n, MitWidget w) => n -> Text -> IO w -> IO (ScrolledWindow, w)
-scrollbaresWidgetNotebookAppendPageNew notebook name konstruktor = do
-    widget <- widgetShowNew konstruktor
-    scrolledWindow <- notebookAppendPageNew notebook name $ widgetShowNew $ scrolledWindowNew Nothing Nothing
-    set scrolledWindow [scrolledWindowHscrollbarPolicy := PolicyNever, scrolledWindowVscrollbarPolicy := PolicyAlways]
-    scrolledWindowAddWithViewport scrolledWindow widget
-    pure (scrolledWindow, widget)
+scrollbaresWidgetNotebookAppendPageNew :: (MonadIO m, MitNotebook n, MitWidget w) => n -> Text -> m w -> m (ScrollbaresWidget w, Int)
+scrollbaresWidgetNotebookAppendPageNew notebook name konstruktor
+    = notebookAppendPageNew notebook name $ scrollbaresWidgetNew konstruktor
+#endif
