38e47482346c521a5672eaf892fda0558af2c018
spamviech <spaviech@web.de>
2019-11-24 23:03:55 +0100

    Fliessend.hs an Anzeige angepasst

--- src/Zug/UI/Gtk/Fliessend.hs
+++ src/Zug/UI/Gtk/Fliessend.hs
@@ -16,12 +16,14 @@ module Zug.UI.Gtk.Fliessend (
 
 import Control.Monad.Trans (MonadIO(..))
 import Data.Text (Text)
+import Graphics.UI.Gtk (AttrOp(..))
 import qualified Graphics.UI.Gtk as Gtk
 -- Abhängigkeiten von anderen Modulen
 import Zug.Anbindung (StreckenAtom(..), Value(..))
-import Zug.Language ((<:>), showText)
+import Zug.Language ((<:>))
 import qualified Zug.Language as Language
-import Zug.UI.Gtk.Hilfsfunktionen (boxPackWidgetNew, boxPackWidgetNewDefault, packingDefault, positionDefault)
+import Zug.UI.Gtk.Hilfsfunktionen (SpracheGuiReader(), verwendeSpracheGui,
+                                    boxPackWidgetNew, boxPackWidgetNewDefault, packingDefault, positionDefault)
 import Zug.UI.Gtk.Klassen (MitWidget(..), MitLabel(..), MitBox(..))
 import Zug.UI.Gtk.Auswahl (AuswahlWidget, boundedEnumAuswahlComboBoxNew, aktuelleAuswahl)
 
@@ -30,25 +32,28 @@ newtype FließendWidget = FließendWidget Gtk.Label
     deriving (Eq, MitWidget, MitLabel)
 
 -- | Erstelle ein neues 'FließendWidget' und packe es in eine 'MitBox'
-fließendPackNew :: (MonadIO m, StreckenAtom s, MitBox b) => b -> s -> m FließendWidget
+fließendPackNew :: (SpracheGuiReader r m, MonadIO m, StreckenAtom s, MitBox b) => b -> s -> m FließendWidget
 fließendPackNew box = boxPackWidgetNew box packingDefault 3 positionDefault . fließendNew
 
 -- | Füge neues 'Label' zu 'Box' hinzu, in dem der 'Value' eines 'StreckenAtom's angezeigt wird, bei dem Strom fließt.
-fließendNew :: (MonadIO m, StreckenAtom s) => s -> m FließendWidget
-fließendNew s = liftIO $ fmap FließendWidget $
-    Gtk.labelNew $ Just $ (Language.fließendValue <:> showText (fließend s) :: Text)
+fließendNew :: (SpracheGuiReader r m, MonadIO m, StreckenAtom s) => s -> m FließendWidget
+fließendNew s = do
+    label <- liftIO $ Gtk.labelNew (Nothing :: Maybe Text)
+    verwendeSpracheGui $
+        \sprache -> Gtk.set label [Gtk.labelText := (Language.fließendValue <:> fließend s) sprache]
+    pure $ FließendWidget label
 
 -- | Widget zur Eingabe des Fließend-Value
 newtype FließendAuswahlWidget = FließendAuswahlWidget {erhalteAuswahlWidget :: AuswahlWidget Value}
     deriving (Eq, MitWidget)
 
 -- | Erstelle ein neues 'FließendAuswahlWidget' und packe es in eine 'MitBox'
-fließendAuswahlPackNew :: (MonadIO m, MitBox b) => b -> m FließendAuswahlWidget
-fließendAuswahlPackNew
-    = flip boxPackWidgetNewDefault fließendAuswahlNew
+fließendAuswahlPackNew :: (SpracheGuiReader r m, MonadIO m, MitBox b) => b -> m FließendAuswahlWidget
+fließendAuswahlPackNew box = boxPackWidgetNewDefault box $ fließendAuswahlNew
 
-fließendAuswahlNew :: (MonadIO m) => m FließendAuswahlWidget
-fließendAuswahlNew = liftIO $ FließendAuswahlWidget <$> boundedEnumAuswahlComboBoxNew LOW Language.fließend
+-- | Erstelle ein neues 'FließendAuswahlWidget'
+fließendAuswahlNew :: (SpracheGuiReader r m, MonadIO m) => m FließendAuswahlWidget
+fließendAuswahlNew = FließendAuswahlWidget <$> boundedEnumAuswahlComboBoxNew LOW Language.fließend
 
 -- | Erhalte den aktuell gewählten Fließend-Value
 aktuellerFließendValue :: (MonadIO m) => FließendAuswahlWidget -> m Value
