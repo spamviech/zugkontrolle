fe643f34d6570294e27573b36b6b231b47e9ebb3
spamviech <spamviech@web.de>
2020-03-19 17:09:26 +0100

    assistantHinzufügenNew-improvements.

--- src/Zug/UI/Gtk/AssistantHinzufuegen.hs
+++ src/Zug/UI/Gtk/AssistantHinzufuegen.hs
@@ -27,16 +27,21 @@ import Control.Monad.Trans (MonadIO(..))
 import Data.Map.Lazy (Map)
 import qualified Data.Map.Lazy as Map
 import qualified Graphics.UI.Gtk as Gtk
+import Graphics.UI.Gtk (AttrOp((:=)))
 
 -- Abhängigkeiten von anderen Modulen
-import Zug.Enums (Zugtyp())
+import Zug.Enums (Zugtyp(), unterstützteZugtypen)
 import Zug.Language (Sprache())
+import qualified Zug.Language as Language
 import Zug.Objekt (Objekt)
 import Zug.UI.Gtk.AssistantHinzufuegen.HinzufuegenSeite (HinzufügenSeite(..), seiteErgebnis)
-import Zug.UI.Gtk.Auswahl (AuswahlWidget, aktuelleAuswahl)
-import Zug.UI.Gtk.Fliessend (FließendAuswahlWidget, aktuellerFließendValue)
-import Zug.UI.Gtk.Klassen (MitWidget(..), mitWidgetShow, mitWidgetHide, MitWindow())
-import Zug.UI.Gtk.StreckenObjekt (ObjektGui)
+import Zug.UI.Gtk.Auswahl (AuswahlWidget, auswahlComboBoxNew, aktuelleAuswahl)
+import Zug.UI.Gtk.Fliessend
+       (FließendAuswahlWidget, fließendAuswahlPackNew, aktuellerFließendValue)
+import Zug.UI.Gtk.Hilfsfunktionen (containerAddWidgetNew, boxPackWidgetNewDefault, boxPackDefault)
+import Zug.UI.Gtk.Klassen (MitWidget(..), mitWidgetShow, mitWidgetHide, MitWindow(..))
+import Zug.UI.Gtk.SpracheGui (SpracheGuiReader(), verwendeSpracheGui)
+import Zug.UI.Gtk.StreckenObjekt (ObjektGui, StatusVarGuiReader)
 import Zug.UI.StatusVar (StatusVarReader(..))
 
 -- | Seiten des Hinzufügen-'Assistant'
@@ -72,7 +77,7 @@ assistantHinzufügenAuswerten AssistantHinzufügen {window, tmVarErgebnis} = lif
     pure ergebnis
 
 -- | Erhalte das Ergebnis einer 'HinzufügenSeite'.
-hinzufügenErgebnis :: (StatusVarReader r ObjektGui m, MonadIO m) => AssistantHinzufügen -> m ()
+hinzufügenErgebnis :: (StatusVarGuiReader r m, MonadIO m) => AssistantHinzufügen -> m ()
 hinzufügenErgebnis
     AssistantHinzufügen {notebook, fließendAuswahl, zugtypAuswahl, indexSeiten, tmVarErgebnis} =
     do
@@ -81,11 +86,36 @@ hinzufügenErgebnis
         liftIO $ atomically $ putTMVar tmVarErgebnis $ HinzufügenErfolgreich ergebnis
 
 -- | Erstelle einen neuen 'AssistantHinzufügen'.
-assistantHinzufügenNew :: (MitWindow p, MonadIO m)
+assistantHinzufügenNew :: (MitWindow p, SpracheGuiReader r m, MonadIO m)
                         => p
                         -> Maybe (TVar (Maybe [Sprache -> IO ()]))
                         -> m AssistantHinzufügen
-assistantHinzufügenNew = do
-    _undefined --TODO
+assistantHinzufügenNew parent maybeTVar = do
+    (window, vBox, notebook) <- liftIO $ do
+        window <- Gtk.windowNew
+        Gtk.set window [Gtk.windowTransientFor := erhalteWindow parent, Gtk.windowModal := True]
+        vBox <- containerAddWidgetNew window $ Gtk.vBoxNew False 0
+        notebook <- boxPackWidgetNewDefault vBox Gtk.notebookNew
+        pure (window, vBox, notebook)
+    zugtypAuswahl <- auswahlComboBoxNew unterstützteZugtypen maybeTVar Language.zugtyp
+    (indexSeiten, functionBox, tmVarErgebnis) <- liftIO $ do
+        indexSeiten <- _undefined --TODO
+        functionBox <- boxPackWidgetNewDefault vBox $ Gtk.hBoxNew False 0
+        tmVarErgebnis <- atomically newEmptyTMVar
+        -- TODO pack hinzufügen-Knöpfe
+        boxPackDefault functionBox zugtypAuswahl
+        pure (indexSeiten, functionBox, tmVarErgebnis)
+    fließendAuswahl <- fließendAuswahlPackNew functionBox maybeTVar
+    verwendeSpracheGui maybeTVar
+        $ \sprache -> Gtk.set window [Gtk.windowTitle := Language.hinzufügen sprache]
+    pure
+        AssistantHinzufügen
+            { window
+            , notebook
+            , fließendAuswahl
+            , zugtypAuswahl
+            , indexSeiten
+            , tmVarErgebnis
+            }
 #endif
 --
