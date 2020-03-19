e6d65781646c5ab4d45888e902112b64f3d2c938
spamviech <spamviech@web.de>
2020-03-19 16:42:18 +0100

    assistantHinzufügenAuswerten

--- src/Zug/UI/Gtk/AssistantHinzufuegen.hs
+++ src/Zug/UI/Gtk/AssistantHinzufuegen.hs
@@ -21,7 +21,8 @@ module Zug.UI.Gtk.AssistantHinzufuegen
 
 #ifdef ZUGKONTROLLEGUI
 -- Bibliotheken
-import Control.Concurrent.STM.TVar (TVar)
+import Control.Concurrent (forkIO)
+import Control.Concurrent.STM (atomically, TVar, TMVar, newEmptyTMVar, putTMVar, takeTMVar)
 import Control.Monad.Trans (MonadIO(..))
 import Data.Map.Lazy (Map)
 import qualified Data.Map.Lazy as Map
@@ -32,9 +33,9 @@ import Zug.Enums (Zugtyp())
 import Zug.Language (Sprache())
 import Zug.Objekt (Objekt)
 import Zug.UI.Gtk.AssistantHinzufuegen.HinzufuegenSeite (HinzufügenSeite(..), seiteErgebnis)
-import Zug.UI.Gtk.Auswahl (AuswahlWidget)
-import Zug.UI.Gtk.Fliessend (FließendAuswahlWidget)
-import Zug.UI.Gtk.Klassen (MitWidget(..), MitWindow())
+import Zug.UI.Gtk.Auswahl (AuswahlWidget, aktuelleAuswahl)
+import Zug.UI.Gtk.Fliessend (FließendAuswahlWidget, aktuellerFließendValue)
+import Zug.UI.Gtk.Klassen (MitWidget(..), mitWidgetShow, mitWidgetHide, MitWindow())
 import Zug.UI.Gtk.StreckenObjekt (ObjektGui)
 import Zug.UI.StatusVar (StatusVarReader(..))
 
@@ -46,6 +47,7 @@ data AssistantHinzufügen =
     , fließendAuswahl :: FließendAuswahlWidget
     , zugtypAuswahl :: AuswahlWidget Zugtyp
     , indexSeiten :: Map Int HinzufügenSeite
+    , tmVarErgebnis :: TMVar HinzufügenErgebnis
     }
     deriving (Eq)
 
@@ -60,22 +62,30 @@ data HinzufügenErgebnis
     | HinzufügenBeenden
 
 -- | Zeige den 'AssistantHinzufügen' an und warte bis ein 'HinzufügenErgebnis' vorliegt.
+--
+-- Es wird erwartet, dass diese Funktion in einem eigenen Thread ausgeführt wird.
 assistantHinzufügenAuswerten :: (MonadIO m) => AssistantHinzufügen -> m HinzufügenErgebnis
-assistantHinzufügenAuswerten = _undefined --TODO
+assistantHinzufügenAuswerten AssistantHinzufügen {window, tmVarErgebnis} = liftIO $ do
+    Gtk.postGUIAsync $ mitWidgetShow window
+    ergebnis <- atomically $ takeTMVar tmVarErgebnis
+    Gtk.postGUIAsync $ mitWidgetHide window
+    pure ergebnis
 
 -- | Erhalte das Ergebnis einer 'HinzufügenSeite'.
+hinzufügenErgebnis :: (StatusVarReader r ObjektGui m, MonadIO m) => AssistantHinzufügen -> m ()
 hinzufügenErgebnis
-    :: (StatusVarReader r ObjektGui m, MonadIO m) => AssistantHinzufügen -> m Objekt
-hinzufügenErgebnis
-    AssistantHinzufügen {notebook, fließendAuswahl, zugtypAuswahl, indexSeiten} = do
-    aktuelleSeite <- liftIO $ Gtk.get notebook Gtk.notebookPage
-    seiteErgebnis fließendAuswahl zugtypAuswahl $ indexSeiten Map.! aktuelleSeite
+    AssistantHinzufügen {notebook, fließendAuswahl, zugtypAuswahl, indexSeiten, tmVarErgebnis} =
+    do
+        aktuelleSeite <- liftIO $ Gtk.get notebook Gtk.notebookPage
+        ergebnis <- seiteErgebnis fließendAuswahl zugtypAuswahl $ indexSeiten Map.! aktuelleSeite
+        liftIO $ atomically $ putTMVar tmVarErgebnis $ HinzufügenErfolgreich ergebnis
 
 -- | Erstelle einen neuen 'AssistantHinzufügen'.
 assistantHinzufügenNew :: (MitWindow p, MonadIO m)
                         => p
                         -> Maybe (TVar (Maybe [Sprache -> IO ()]))
                         -> m AssistantHinzufügen
-assistantHinzufügenNew = _undefined --TODO
+assistantHinzufügenNew = do
+    _undefined --TODO
 #endif
- --
+--
