ca44fe0614b2ce68fe6cfb6e2ee3280849c0095b
spamviech <spamviech@web.de>
2020-05-22 09:58:27 +0200

    Warteschlange über Data.Sequence implementiert.

--- src/Zug/Warteschlange.hs
+++ src/Zug/Warteschlange.hs
@@ -17,27 +17,27 @@ module Zug.Warteschlange
   ) where
 
 import Data.Foldable (toList)
+import Data.Sequence (Seq((:<|), (:|>)))
+import qualified Data.Sequence as Seq
 import Data.Text (Text)
+import Prelude hiding (seq)
 
 import qualified Zug.Language as Language
 
--- | Single-Ended Queue
-data Warteschlange a = Warteschlange { eingabe :: [a], ausgabe :: [a] }
+-- | Single-Ended Queue. Ein auslesen ist von beiden Seiten der Warteschlange möglich.
+newtype Warteschlange a = Warteschlange { seq :: Seq a }
 
 instance Foldable Warteschlange where
     foldMap :: Monoid m => (a -> m) -> Warteschlange a -> m
-    foldMap f seQueue = case zeigeErstes seQueue of
-        Leer -> mempty
-        (Gefüllt h t) -> mappend (f h) $ foldMap f t
+    foldMap f = foldMap f . seq
 
 instance Functor Warteschlange where
     fmap :: (a -> b) -> Warteschlange a -> Warteschlange b
-    fmap f Warteschlange {eingabe = e1, ausgabe = a1} =
-        Warteschlange { eingabe = f <$> e1, ausgabe = f <$> a1 }
+    fmap f = Warteschlange . fmap f . seq
 
 instance (Show a) => Show (Warteschlange a) where
     show :: Warteschlange a -> String
-    show = show . toList
+    show = show . seq
 
 instance (Language.Anzeige a) => Language.Anzeige (Warteschlange a) where
     anzeige :: Warteschlange a -> Language.Sprache -> Text
@@ -45,43 +45,37 @@ instance (Language.Anzeige a) => Language.Anzeige (Warteschlange a) where
 
 instance (Eq a) => Eq (Warteschlange a) where
     (==) :: Warteschlange a -> Warteschlange a -> Bool
-    a == b = toList a == toList b
+    a == b = seq a == seq b
 
--- | Ergebnis-Typ von 'zeigeErstes' und 'zeigeLetztes'
+-- | Ergebnis-Typ von 'zeigeErstes' und 'zeigeLetztes'.
 data Anzeige a
     = Leer
     | Gefüllt a (Warteschlange a)
 
 -- | Leere 'Warteschlange'
 leer :: Warteschlange a
-leer = Warteschlange { eingabe = [], ausgabe = [] }
+leer = Warteschlange { seq = Seq.Empty }
 
 -- | 'Warteschlange' mit genau einem Element
 einzelElement :: a -> Warteschlange a
-einzelElement a = Warteschlange { eingabe = [], ausgabe = [a] }
+einzelElement = Warteschlange . Seq.singleton
 
--- | Erstelle 'Warteschlange' aus einer Liste. Der Kopf der Liste ist das erste Element in der Warteschlange.
+-- | Erstelle 'Warteschlange' aus einer Liste.
+-- Der Kopf der Liste ist das erste Element in der Warteschlange.
 vonListe :: [a] -> Warteschlange a
-vonListe ausgabe = Warteschlange { eingabe = [], ausgabe }
+vonListe = Warteschlange . Seq.fromList
 
 -- | Hänge ein Element an eine 'Warteschlange' an. Effizienz ist O(1).
 anhängen :: a -> Warteschlange a -> Warteschlange a
-anhängen a Warteschlange {eingabe = e1, ausgabe} = Warteschlange { eingabe = a : e1, ausgabe }
+anhängen a Warteschlange {seq} = Warteschlange { seq = seq :|> a }
 
 -- | Erhalte das erste Element und die Verbleibende 'Warteschlange'. Amortisierte Effizienz ist O(1).
 zeigeErstes :: Warteschlange a -> Anzeige a
-zeigeErstes Warteschlange {eingabe = [], ausgabe = []} = Leer
-zeigeErstes Warteschlange {eingabe, ausgabe = []} =
-    let (a:ausgabe) = reverse eingabe
-    in Gefüllt a Warteschlange { eingabe = [], ausgabe }
-zeigeErstes Warteschlange {eingabe, ausgabe = (h:t)} =
-    Gefüllt h Warteschlange { eingabe, ausgabe = t }
+zeigeErstes Warteschlange {seq = a :<| seq} = Gefüllt a Warteschlange { seq }
+zeigeErstes Warteschlange {seq = Seq.Empty} = Leer
 
 -- | Erhalte das zuletzt hinzugefügte Element und die 'Warteschlange' vor hinzufügen des selben.
 -- Effizienz ist im schlimmsten Fall O(n).
 zeigeLetztes :: Warteschlange a -> Anzeige a
-zeigeLetztes Warteschlange {eingabe = [], ausgabe = []} = Leer
-zeigeLetztes Warteschlange {eingabe = [], ausgabe = a1} =
-    Gefüllt (last a1) Warteschlange { eingabe = [], ausgabe = init a1 }
-zeigeLetztes Warteschlange {eingabe = (h:t), ausgabe} =
-    Gefüllt h Warteschlange { eingabe = t, ausgabe }
+zeigeLetztes Warteschlange {seq = seq :|> a} = Gefüllt a Warteschlange { seq }
+zeigeLetztes Warteschlange {seq = Seq.Empty} = Leer
