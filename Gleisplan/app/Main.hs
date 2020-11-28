ab4172e6e5fdf9afc86c2b50d84f1f82a81040c9
spamviech <spamviech@web.de>
2020-11-28 12:35:29 +0100

    added gtk-examples

---
+++ Gleisplan/app/Main.hs
@@ -0,0 +1,29 @@
+{-# LANGUAGE MonoLocalBinds #-}
+
+module Main (main) where
+
+import qualified GI.Gio as Gio
+import qualified GI.Gtk as Gtk
+
+import Zug.UI.Gtk.Gleise (gleisAnzeigeNew)
+
+main :: IO ()
+main = do
+    application <- Gtk.applicationNew Nothing []
+    Gio.onApplicationActivate application $ createWindow application
+    Gio.applicationRun application Nothing
+    pure ()
+
+createWindow :: (Gtk.IsApplication a) => a -> IO ()
+createWindow application = do
+    appWindow <- Gtk.applicationWindowNew application
+    -- Hide decoration, since for some reason Gtk puts another window around it
+    -- main window (Application) is really unresponsive for some reasons
+    --Gtk.windowSetDecorated appWindow False
+    Gtk.applicationAddWindow application appWindow
+    Gtk.windowSetDefaultSize appWindow 480 320
+    scrolledWindow <- Gtk.scrolledWindowNew
+    Gtk.windowSetChild appWindow $ Just scrolledWindow
+    gleisAnzeige <- gleisAnzeigeNew
+    Gtk.scrolledWindowSetChild scrolledWindow $ Just gleisAnzeige
+    Gtk.widgetShow appWindow
