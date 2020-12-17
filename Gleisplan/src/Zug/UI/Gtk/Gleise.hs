2f0278e430b4e1c005c8ac9f5bb75df5d44bf21f
spamviech <spamviech@web.de>
2020-12-18 00:54:52 +0100

    Daten Lego-Eisenbahn

--- Gleisplan/src/Zug/UI/Gtk/Gleise.hs
+++ Gleisplan/src/Zug/UI/Gtk/Gleise.hs
@@ -628,8 +628,28 @@ gleisAnzeigeNew = do
             Gtk.fixedPut fixed drawingArea (fromIntegral x) $ fromIntegral y
             pure (max (x + width) maxWidth, y + height + padding)
 
+-- https://blaulicht-schiene.jimdofree.com/projekte/lego-daten/
+{-
+ Der Maßstab liegt zwischen 1:35 - 1:49.
+Spurbreite
+    Innen: ca. 3,81 cm (38,1 mm)
+    Außen: ca. 4,20 cm (42,0 mm)
+Gerade (Straight)
+    Länge: ca. 17 Noppen / 13,6 cm
+    Breite: ca. 8 Noppen / 6,4 cm
+    Höhe: ca. 1 1/3 Noppen (1 Stein-Höhe) / 1,0 cm
+Kurve (Curve)
+    Länge Außen: ca. 17 1/2 Noppen (14 1/3 Legosteine hoch) / 13,7 cm
+    Länge Innen: ca. 15 1/6 Noppen (12 2/3 Legosteine hoch) / 12,1 cm
+    Breite: 8 Noppen / 6,4 cm
+Kreis-Aufbau mit PF-Kurven
+Ein Kreis benötigt 16 Lego-PF-Kurven.
+    Kreis-Durchmesser Außen: ca. 88 Noppen / 70 cm
+    Kreis-Durchmesser Innen: ca. 72 Noppen / 57 cm
+    Kreis-Radius (halber Kreis, Außen bis Kreismittelpunkt): ca. 44 Noppen / 35 cm 
+-}
 {-
 Lego Spurweite: 38mm
 -}
 legoGeradeNew :: (MonadIO m) => m (Gleis 'Lego)
-legoGeradeNew = geradeNew $ error "Geraden-Länge"
+legoGeradeNew = geradeNew 13.6
