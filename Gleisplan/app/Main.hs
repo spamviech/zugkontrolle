d47fa31695a135c2afdc023d0cf8aa097b064eeb
spamviech <spamviech@web.de>
2021-01-10 13:33:14 +0100

    legacyController for testing Events
    * If applicable, provided specialized EventController (e.g. EventControllerClick) should be used
    * keys only work directly from appWindow
    * I could't figue out a way to get the MouseButton using GestureClick
    * unsafeCastTo required, since ButtonEvent, etc. don't have a GType instance

--- Gleisplan/app/Main.hs
+++ Gleisplan/app/Main.hs
@@ -1,8 +1,11 @@
 {-# LANGUAGE MonoLocalBinds #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE TupleSections #-}
 
 module Main (main) where
 
 import Control.Monad (void)
+import qualified GI.Gdk as Gdk
 import qualified GI.Gio as Gio
 import qualified GI.Gtk as Gtk
 
@@ -19,6 +22,55 @@ createWindow :: (Gtk.IsApplication a) => a -> IO ()
 createWindow application = do
     appWindow <- Gtk.applicationWindowNew application
     Gtk.applicationAddWindow application appWindow
-    Gtk.windowSetDefaultSize appWindow 480 320
     Gtk.windowSetChild appWindow . Just =<< erhalteWidget =<< gleisDemonstrationNew
+    -- test legacy events
+    -- key events only happen in appWindow, they are not passed to children
+    legacyController <- Gtk.eventControllerLegacyNew
+    Gtk.onEventControllerLegacyEvent legacyController $ \event -> do
+        maybeMsg <- Gdk.eventGetEventType event >>= \case
+            Gdk.EventTypeButtonPress -> do
+                buttonEvent <- Gdk.unsafeCastTo Gdk.ButtonEvent event
+                Just . ("pressed button: " <>) . show <$> Gdk.buttonEventGetButton buttonEvent
+            Gdk.EventTypeButtonRelease -> do
+                buttonEvent <- Gdk.unsafeCastTo Gdk.ButtonEvent event
+                Just . ("released button: " <>) . show <$> Gdk.buttonEventGetButton buttonEvent
+            Gdk.EventTypeKeyPress -> do
+                keyEvent <- Gdk.unsafeCastTo Gdk.KeyEvent event
+                keyval <- Gdk.keyEventGetKeyval keyEvent
+                keycode <- Gdk.keyEventGetKeycode keyEvent
+                let knownKey = case fromIntegral keyval of
+                        Gdk.KEY_Up -> Just "up"
+                        Gdk.KEY_Down -> Just "down"
+                        Gdk.KEY_Left -> Just "left"
+                        Gdk.KEY_Right -> Just "right"
+                        _other -> Nothing
+                -- keycode represents state of shift, ctrl, etc.
+                pure
+                    $ Just
+                    $ "pressed key (keyval, keycode): ("
+                    <> show keyval
+                    <> maybe "" (": " <>) knownKey
+                    <> ", "
+                    <> show keycode
+                    <> ")"
+            Gdk.EventTypeKeyRelease -> do
+                keyEvent <- Gdk.unsafeCastTo Gdk.KeyEvent event
+                fmap (Just . ("released key (keyval, keycode): " <>) . show)
+                    $ (,) <$> Gdk.keyEventGetKeyval keyEvent <*> Gdk.keyEventGetKeycode keyEvent
+            Gdk.EventTypeScroll -> do
+                scrollEvent <- Gdk.unsafeCastTo Gdk.ScrollEvent event
+                fmap (Just . ("scroll event (deltas, direction, isStop): " <>) . show)
+                    $ (,,) <$> Gdk.scrollEventGetDeltas scrollEvent
+                    <*> Gdk.scrollEventGetDirection scrollEvent
+                    <*> Gdk.scrollEventIsStop scrollEvent
+            Gdk.EventTypeMotionNotify -> pure Nothing    -- main occurring event, clutters console
+            eventType -> pure $ Just $ show eventType
+        case maybeMsg of
+            (Just msg) -> do
+                putStrLn "----------"
+                print =<< Gdk.eventGetPosition event
+                putStrLn msg
+            Nothing -> pure ()
+        pure False
+    Gtk.widgetAddController appWindow legacyController
     Gtk.widgetShow appWindow
