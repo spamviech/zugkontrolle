2e2bf6d3cbfa8f7c5c97eae2abb391b333ce1d2d
spamviech <spamviech@web.de>
2021-03-21 00:45:44 +0100

    include gtk example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use gio::prelude::*;
+use gtk::prelude::*;
+use gtk::{Application, ApplicationWindow, Button};
+
 fn main() {
-    println!("Hello, world!");
+    let application =
+        Application::new(Some("com.github.gtk-rs.examples.basic"), Default::default())
+            .expect("failed to initialize GTK application");
+
+    application.connect_activate(|app| {
+        let window = ApplicationWindow::new(app);
+        window.set_title("First GTK+ Program");
+        window.set_default_size(350, 70);
+
+        let button = Button::with_label("Click me!");
+        button.connect_clicked(|_| {
+            println!("Clicked!");
+        });
+        window.add(&button);
+
+        window.show_all();
+    });
+
+    application.run(&[]);
 }
