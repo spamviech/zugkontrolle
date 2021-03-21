2285217d73e7a8bfdd6028cdac2d0c54c02d5804
spamviech <spamviech@web.de>
2021-03-21 14:30:46 +0100

    adventures with GStrings

--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,23 @@ use gtk::{Application, ApplicationWindow, Button};
 
 fn main() {
     let application =
-        Application::new(Some("com.github.gtk-rs.examples.basic"), Default::default())
-            .expect("failed to initialize GTK application");
+        Application::new(None, Default::default()).expect("failed to initialize GTK application");
 
     application.connect_activate(|app| {
         let window = ApplicationWindow::new(app);
-        window.set_title("First GTK+ Program");
-        window.set_default_size(350, 70);
+        window.set_title("Zugkontrolle");
+        window.set_default_size(600, 400);
 
         let button = Button::with_label("Click me!");
-        button.connect_clicked(|_| {
-            println!("Clicked!");
+        button.connect_clicked(|b| {
+            let next = match b.get_label() {
+                Some(g) => match g.as_str() {
+                    "Click me!" => "Click me again!",
+                    _ => "Click me!",
+                },
+                None => panic!("Button without label"),
+            };
+            b.set_label(next);
         });
         window.add(&button);
 
