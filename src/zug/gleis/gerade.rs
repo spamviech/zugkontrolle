592ed6f88984bf2834ad228c5d69bcdcb4f5a3f2
spamviech <spamviech@web.de>
2021-03-22 13:46:31 +0100

    refactor

--- src/zug/gleis/gerade.rs
+++ src/zug/gleis/gerade.rs
@@ -31,37 +31,57 @@ impl<Z: Zugtyp> Gerade<Z> {
     }
 
     pub fn zeichne(&self, c: Context) {
-        let gleis_links: CanvasX = CanvasX::default();
-        let gleis_rechts: CanvasX = CanvasX::default() + CanvasAbstand::new(self.length.0);
-        let beschraenkung_oben: CanvasY = CanvasY::default();
-        let beschraenkung_unten: CanvasY = CanvasY::default() + Z::beschraenkung;
         // Beschränkungen
-        c.move_to(gleis_links.0, beschraenkung_oben.0);
-        c.line_to(gleis_links.0, beschraenkung_unten.0);
-        c.move_to(gleis_rechts.0, beschraenkung_oben.0);
-        c.line_to(gleis_rechts.0, beschraenkung_unten.0);
+        c.move_to(self.gleis_links().0, self.beschraenkung_oben().0);
+        c.line_to(self.gleis_links().0, self.beschraenkung_unten().0);
+        c.move_to(self.gleis_rechts().0, self.beschraenkung_oben().0);
+        c.line_to(self.gleis_rechts().0, self.beschraenkung_unten().0);
         // Gleis
-        let gleis_oben: CanvasY = CanvasY::default() + Z::abstand;
-        let gleis_unten: CanvasY = CanvasY::default() + Z::beschraenkung;
-        c.move_to(gleis_links.0, gleis_oben.0);
-        c.line_to(gleis_rechts.0, gleis_oben.0);
-        c.move_to(gleis_links.0, gleis_unten.0);
-        c.line_to(gleis_rechts.0, gleis_unten.0)
+        c.move_to(self.gleis_links().0, self.gleis_oben().0);
+        c.line_to(self.gleis_rechts().0, self.gleis_oben().0);
+        c.move_to(self.gleis_links().0, self.gleis_unten().0);
+        c.line_to(self.gleis_rechts().0, self.gleis_unten().0)
     }
 
     pub fn anchor_points(&self) -> AnchorPointMap {
-        let gleis_links: CanvasX = CanvasX::default();
-        let gleis_rechts: CanvasX = CanvasX::default() + CanvasAbstand::new(self.length.0);
-        let beschraenkung_mitte: CanvasY = CanvasY::default() + 0.5 * Z::beschraenkung;
         with_anchor_name("Gerade", [
             AnchorPoint {
-                position: AnchorPosition { x: gleis_links, y: beschraenkung_mitte },
+                position: AnchorPosition { x: self.gleis_links(), y: self.beschraenkung_mitte() },
                 direction: AnchorDirection { dx: CanvasX(-1.), dy: CanvasY(0.) },
             },
             AnchorPoint {
-                position: AnchorPosition { x: gleis_rechts, y: beschraenkung_mitte },
+                position: AnchorPosition { x: self.gleis_rechts(), y: self.beschraenkung_mitte() },
                 direction: AnchorDirection { dx: CanvasX(1.), dy: CanvasY(0.) },
             },
         ])
+    }
+
+    // Utility functions
+    fn gleis_links(&self) -> CanvasX {
+        CanvasX::default()
+    }
+
+    fn gleis_rechts(&self) -> CanvasX {
+        CanvasX::default() + CanvasAbstand::new(self.length.0)
+    }
+
+    fn beschraenkung_mitte(&self) -> CanvasY {
+        CanvasY::default() + 0.5 * Z::beschraenkung
+    }
+
+    fn beschraenkung_oben(&self) -> CanvasY {
+        CanvasY::default()
+    }
+
+    fn beschraenkung_unten(&self) -> CanvasY {
+        CanvasY::default() + Z::beschraenkung
+    }
+
+    fn gleis_oben(&self) -> CanvasY {
+        CanvasY::default() + Z::abstand
+    }
+
+    fn gleis_unten(&self) -> CanvasY {
+        CanvasY::default() + Z::beschraenkung
     }
 }
