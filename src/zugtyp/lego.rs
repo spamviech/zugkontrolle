59b1b3ae83842b451c3a58308c9ba7eeb83f2d31
spamviech <spamviech@web.de>
2021-05-10 15:37:42 +0200

    korrigiere Lego-Weiche

--- src/zugtyp/lego.rs
+++ src/zugtyp/lego.rs
@@ -66,7 +66,8 @@ Lego Spurweite: 38mm
 */
 const LENGTH_VALUE: f32 = 128.;
 const LENGTH: Länge = Länge::neu(LENGTH_VALUE);
-const RADIUS: Radius = Radius::neu(320.);
+const RADIUS_VALUE: f32 = 320.;
+const RADIUS: Radius = Radius::neu(RADIUS_VALUE);
 const ANGLE_VALUE_DEGREE: f32 = 22.5;
 const ANGLE_VALUE: f32 = ANGLE_VALUE_DEGREE * PI / 180.;
 
@@ -80,6 +81,9 @@ Eine leichte S-Kurve: 6.5 Lücken rechts, dann 2.5 Lücken links; insgesamt 22.5
 Normale Kurve (22.5°) hat 4 Lücken
 Nach 1 Gerade/Kurve sind Haupt- und Parallelgleis auf der selben Höhe
 // FIXME aktueller Wert stimmt nicht ganz!
+const ANGLE_DIFFERENCE: Winkel = ANGLE_OUTWARDS - ANGLE_INWARDS;
+ANGLE_OUTWARDS = (1.5 * LENGTH / RADIUS).asin();
+ANGLE_INWARDS = ANGLE_OUTWARDS - ANGLE;
 -------------------------------------------------------
 Die Geometrie der LEGO Eisenbahnweichen hat sich mit Einführung des 9 V Systems verändert.
 Das Parallelgleis nach der Weiche ist nun 8 Noppen vom Hauptgleis entfernt.
@@ -89,8 +93,11 @@ Beim 4,5 V/12V System führte das Parallelgleis direkt am Hauptgleis entlang.
 1,00 cm sind rund 1,25 Noppen (genauer: 1,255...)
 */
 const DOUBLE_LENGTH: Länge = Länge::neu(2. * LENGTH_VALUE);
-const ANGLE_OUTWARDS: Winkel = Winkel(1.625 * ANGLE_VALUE);
-const ANGLE_INWARDS: Winkel = Winkel(0.625 * ANGLE_VALUE);
+// const ANGLE_OUTWARDS_VALUE: f32 = (1.5 * LENGTH_VALUE / RADIUS_VALUE).asin();
+// https://www.wolframalpha.com/input/?i=asin%281.5+*+128%2F+320%29
+const ANGLE_OUTWARDS_VALUE: f32 = 0.6435011087932843868028092287173226380415105911153123828656;
+const ANGLE_OUTWARDS: Winkel = Winkel(ANGLE_OUTWARDS_VALUE);
+const ANGLE_INWARDS: Winkel = Winkel(ANGLE_OUTWARDS_VALUE - ANGLE_VALUE);
 pub const fn weiche(richtung: weiche::Richtung) -> SKurvenWeiche<Lego> {
     SKurvenWeiche::neu(DOUBLE_LENGTH, RADIUS, ANGLE_OUTWARDS, RADIUS, ANGLE_INWARDS, richtung)
 }
