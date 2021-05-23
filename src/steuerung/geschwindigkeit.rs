8072857ece5814212c24b10f485f2a52b0b96de1
spamviech <spamviech@web.de>
2021-05-23 15:28:34 +0200

    stub-Methoden Geschwindigkeit

--- src/steuerung/geschwindigkeit.rs
+++ src/steuerung/geschwindigkeit.rs
@@ -1,7 +1,49 @@
 //! Einstellen der Geschwindigkeit.
 
-// Geschwindigkeit: Pwm / Mehrere Anschlüsse mit konstanter Spannung
-// Umdrehen:
-//      Märklin: Pwm->Kein Extra-Anschluss, KS->Extra-Anschluss (Überspannung)
-//      Lego: Immer Extra-Anschluss (High-Low an Schienen wechseln)
-pub struct Geschwindigkeit;
+use non_empty_vec::NonEmpty;
+
+use crate::anschluss::*;
+
+#[derive(Debug)]
+pub struct Geschwindigkeit<Leiter> {
+    pub name: String,
+    pub leiter: Leiter,
+}
+
+#[derive(Debug)]
+pub enum Mittelleiter {
+    Pwm { anschluss: pwm::Pin },
+    KonstanteSpannung { geschwindigkeit: NonEmpty<OutputAnschluss>, umdrehen: OutputAnschluss },
+}
+
+impl Geschwindigkeit<Mittelleiter> {
+    pub fn geschwindigkeit(&mut self, geschwindigkeit: u8) -> Result<(), Error> {
+        todo!()
+    }
+
+    pub fn umdrehen(&mut self) -> Result<(), Error> {
+        todo!()
+    }
+}
+
+#[derive(Debug)]
+pub enum Zweileiter {
+    Pwm { geschwindigkeit: pwm::Pin, fahrtrichtung: OutputAnschluss },
+    KonstanteSpannung { geschwindigkeit: NonEmpty<OutputAnschluss>, fahrtrichtung: OutputAnschluss },
+}
+
+impl Geschwindigkeit<Zweileiter> {
+    pub fn geschwindigkeit(&mut self, geschwindigkeit: u8) -> Result<(), Error> {
+        todo!()
+    }
+
+    pub fn fahrtrichtung(&mut self, fahrtrichtung: Fahrtrichtung) -> Result<(), Error> {
+        todo!()
+    }
+}
+
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+pub enum Fahrtrichtung {
+    Vorwärts,
+    Rückwärts,
+}
