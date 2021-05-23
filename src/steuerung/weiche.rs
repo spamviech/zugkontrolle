7f4d0ec07a6f43e2769c9496813b3dbaa308cf90
spamviech <spamviech@web.de>
2021-05-23 15:22:27 +0200

    NonEmpty hat as_mut_slice-Methode

--- src/steuerung/weiche.rs
+++ src/steuerung/weiche.rs
@@ -2,19 +2,20 @@
 
 use std::{thread::sleep, time::Duration};
 
+use non_empty_vec::NonEmpty;
+
 use crate::anschluss::{Error, Fließend, OutputAnschluss};
 
 // inklusive Kreuzung
 pub struct Weiche<Richtung> {
     // TODO name ist eigentlich nur für die Anzeige relevant
     pub name: String,
-    // TODO non-empty-vec hat Probleme bei iter_mut
-    pub anschlüsse: Vec<(Richtung, OutputAnschluss)>,
+    pub anschlüsse: NonEmpty<(Richtung, OutputAnschluss)>,
 }
 
 impl<Richtung: PartialEq> Weiche<Richtung> {
     pub fn schalten(&mut self, neue_richtung: &Richtung) -> Result<(), Error> {
-        for (richtung, anschluss) in self.anschlüsse.iter_mut() {
+        for (richtung, anschluss) in self.anschlüsse.as_mut_slice() {
             if &*richtung == neue_richtung {
                 anschluss.einstellen(Fließend::Fließend)?;
                 sleep(SCHALTZEIT);
