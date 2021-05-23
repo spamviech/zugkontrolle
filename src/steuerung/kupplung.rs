9d0866c0a788bbbc390e01b2d4e9f6a7a6d0292f
spamviech <spamviech@web.de>
2021-05-23 10:24:12 +0200

    kupplung impl

--- src/steuerung/kupplung.rs
+++ src/steuerung/kupplung.rs
@@ -1,3 +1,23 @@
 //! Kupplungs-Schienen zum trennen von Wagons.
 
-pub struct Kupplung;
+use std::{thread::sleep, time::Duration};
+
+use crate::anschluss::{Error, Fließend, OutputAnschluss, Polarity};
+
+pub struct Kupplung {
+    // TODO name,farbe sind eigentlich nur für die Anzeige relevant
+    pub name: String,
+    pub anschluss: OutputAnschluss,
+    pub polarität: Polarity,
+}
+
+impl Kupplung {
+    pub fn kuppeln(&mut self) -> Result<(), Error> {
+        self.anschluss.write(Fließend::Fließend.with_polarity(self.polarität))?;
+        sleep(KUPPELZEIT);
+        self.anschluss.write(Fließend::Gesperrt.with_polarity(self.polarität))
+    }
+}
+
+// TODO als Teil des Zugtyp-Traits?
+const KUPPELZEIT: Duration = Duration::from_millis(500);
