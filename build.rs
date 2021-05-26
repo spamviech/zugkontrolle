48889ef6a8b133bce4fe21dbdcd7a53636e36e33
spamviech <spamviech@web.de>
2021-05-27 00:39:55 +0200

    fix cfg alias

--- build.rs
+++ build.rs
@@ -3,7 +3,12 @@ use cfg_aliases::cfg_aliases;
 fn main() {
     // Setup cfg aliases
     cfg_aliases! {
-        // Raspberry Pi 2/3/4 or 0/1
-        raspi: { any(target = "armv7-unknown-linux-gnueabihf", target = "arm-unknown-linux-gnueabihf") },
+        // Raspberry Pi 0/1 or 2/3/4
+        raspi: { all(
+                    any(target_arch = "arm", target_arch = "aarch64"),
+                    target_vendor = "unknown",
+                    target_os = "linux",
+                    target_env = "gnu"
+                ) },
     }
 }
