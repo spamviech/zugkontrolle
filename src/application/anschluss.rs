1cbf28b05ed8c2958d92e37866665957d2f6afea
spamviech <spamviech@web.de>
2021-05-27 23:15:59 +0200

    anschluss-Auswahl Widget

--- src/application/anschluss.rs
+++ src/application/anschluss.rs
@@ -1,37 +1,150 @@
 //! Auswahl eines Anschlusses.
 
-use iced::{Element, Length, Text};
-use iced_aw::{TabLabel, Tabs};
+use iced::{button, Button, Column, Element, Length, Radio, Row, Text};
+use iced_aw::{number_input, NumberInput, TabLabel, Tabs};
+use num_x::u3;
 
+use crate::anschluss::{
+    anschlüsse::Error, level::Level, pcf8574::Variante, Anschluss, Anschlüsse
+};
 mod style;
 
+// TODO direkt Input/Output/Pwm-Varianten
 #[derive(Debug)]
 pub struct Auswahl {
     active_tab: usize,
+    confirm_state: button::State,
+    pin_state: number_input::State,
+    pin: u8,
+    a0: Level,
+    a1: Level,
+    a2: Level,
+    variante: Variante,
+    port_state: number_input::State,
+    port: u8,
 }
 
+#[derive(Debug, Clone)]
 pub enum Message {
     TabSelected(usize),
+    Pin(u8),
+    A0(Level),
+    A1(Level),
+    A2(Level),
+    Variante(Variante),
+    Port(u8),
+    Hinzufügen,
 }
 
 impl Auswahl {
+    pub fn neu() -> Self {
+        Auswahl {
+            active_tab: 0,
+            confirm_state: button::State::new(),
+            pin_state: number_input::State::new(),
+            pin: 0,
+            a0: Level::Low,
+            a1: Level::Low,
+            a2: Level::Low,
+            variante: Variante::Normal,
+            port_state: number_input::State::new(),
+            port: 0,
+        }
+    }
+
     pub fn view(&mut self) -> Element<Message> {
+        let Auswahl {
+            active_tab,
+            confirm_state,
+            pin_state,
+            pin,
+            a0,
+            a1,
+            a2,
+            variante,
+            port_state,
+            port,
+        } = self;
         let tabs = vec![
             (
                 TabLabel::Text("Pin".to_string()),
-                // TODO
-                Text::new("TODO").into(),
+                NumberInput::new(pin_state, *pin, 32, Message::Pin).into(),
             ),
             (
                 TabLabel::Text("Pcf8574-Port".to_string()),
-                // TODO
-                Text::new("TODO").into(),
+                Row::new()
+                    .push(
+                        Column::new()
+                            .push(Radio::new(Level::High, "H", Some(*a0), Message::A0))
+                            .push(Radio::new(Level::Low, "L", Some(*a0), Message::A0)),
+                    )
+                    .push(
+                        Column::new()
+                            .push(Radio::new(Level::High, "H", Some(*a1), Message::A1))
+                            .push(Radio::new(Level::Low, "L", Some(*a1), Message::A1)),
+                    )
+                    .push(
+                        Column::new()
+                            .push(Radio::new(Level::High, "H", Some(*a2), Message::A2))
+                            .push(Radio::new(Level::Low, "L", Some(*a2), Message::A2)),
+                    )
+                    .push(
+                        Column::new()
+                            .push(Radio::new(
+                                Variante::Normal,
+                                "Normal",
+                                Some(*variante),
+                                Message::Variante,
+                            ))
+                            .push(Radio::new(Variante::A, "A", Some(*variante), Message::Variante)),
+                    )
+                    .push(NumberInput::new(port_state, *port, 8, Message::Port))
+                    .into(),
             ),
         ];
-        Tabs::with_tabs(self.active_tab, tabs, Message::TabSelected)
-            .tab_bar_style(style::TabBar)
+        Column::new()
+            .push(
+                Tabs::with_tabs(*active_tab, tabs, Message::TabSelected)
+                    .tab_bar_style(style::TabBar),
+            )
+            .push(Element::from(
+                Button::new(confirm_state, Text::new("Hinzufügen")).on_press(Message::Hinzufügen),
+            ))
             .width(Length::Fill)
             .height(Length::Fill)
             .into()
+    }
+
+    pub fn update(&mut self, message: Message) -> Option<Result<Anschluss, Error>> {
+        use Message::*;
+        let mut result: Option<Result<Anschluss, Error>> = None;
+        match message {
+            TabSelected(tab) => self.active_tab = tab,
+            Pin(pin) => self.pin = pin,
+            A0(a0) => self.a0 = a0,
+            A1(a1) => self.a1 = a1,
+            A2(a2) => self.a2 = a2,
+            Variante(variante) => self.variante = variante,
+            Port(port) => self.port = port,
+            Hinzufügen => {
+                result = Some(Anschlüsse::neu().and_then(|mut anschlüsse| {
+                    if self.active_tab == 0 {
+                        anschlüsse.reserviere_pin(self.pin).map(Into::into)
+                    } else {
+                        anschlüsse
+                            .reserviere_pcf8574_port(
+                                self.a0,
+                                self.a1,
+                                self.a2,
+                                self.variante,
+                                u3::new(self.port),
+                            )
+                            .map(Into::into)
+                            .map_err(Into::into)
+                    }
+                }))
+            },
+        }
+        result
     }
 }
