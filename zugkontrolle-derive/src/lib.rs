b3de86562b8acf3ecbb09fbe23172f441c888fab
spamviech <spamviech@web.de>
2021-06-02 15:23:34 +0200

    fix impl_lookup struct-Element

--- zugkontrolle-derive/src/lib.rs
+++ zugkontrolle-derive/src/lib.rs
@@ -24,15 +24,6 @@ pub fn clone_derive(input: TokenStream) -> TokenStream {
 }
 
 mod lookup;
-#[proc_macro_derive(Lookup)]
-pub fn anchor_lookup_derive(input: TokenStream) -> TokenStream {
-    // Construct a representation of Rust code as a syntax tree
-    // that we can manipulate
-    let ast = syn::parse(input).expect("Failed to parse input!");
-
-    // Build the trait implementation
-    lookup::impl_anchor_lookup(&ast).into()
-}
 #[proc_macro_attribute]
 pub fn impl_lookup(attr: TokenStream, item: TokenStream) -> TokenStream {
     let args = syn::parse_macro_input!(attr);
--- zugkontrolle-derive/src/lookup.rs
+++ zugkontrolle-derive/src/lookup.rs
@@ -6,72 +6,6 @@ use proc_macro2::TokenStream;
 use proc_macro_crate::{crate_name, FoundCrate};
 use quote::{format_ident, quote};
 
-pub fn impl_anchor_lookup(ast: &syn::DeriveInput) -> TokenStream {
-    let syn::DeriveInput { ident, data, vis, .. } = ast;
-
-    let enum_data = if let syn::Data::Enum(enum_data) = data {
-        enum_data
-    } else {
-        let error_message = format!("Not an enum: {:?}", ast);
-        return quote! {
-            compile_error!(#error_message);
-        }
-    };
-
-    let base_ident: syn::Ident =
-        match crate_name("zugkontrolle").expect("zugkontrolle missing in `Cargo.toml`") {
-            FoundCrate::Itself => format_ident!("{}", "crate"),
-            FoundCrate::Name(name) => format_ident!("{}", name),
-        };
-    let enum_name: &syn::Ident = ident;
-    let enum_vis: &syn::Visibility = vis;
-    let enum_variants: Vec<syn::Ident> =
-        enum_data.variants.iter().map(|v| v.ident.clone()).collect();
-    // construct a struct using a snake_case field for every variant, each holding an
-    // anchor::Anchor
-    let struct_name: syn::Ident =
-        format_ident!("{}Points", enum_name.to_string().trim_end_matches("Name"));
-    let struct_fields: Vec<syn::Ident> = enum_variants
-            .iter()
-            // TODO fix upstream?
-            // to_snakecase wrongly adds a '_' before 'ß', even though it it a small letter
-            // possibly because there is no real uppercase character of it
-            .map(|ident| format_ident!("{}", to_snake_case(&ident.to_string()).replace("_ß", "ß")))
-            .collect();
-    let struct_definition: TokenStream = quote! {
-        #[derive(Debug)]
-        #enum_vis struct #struct_name {
-            #(pub #struct_fields : #base_ident::application::gleis::anchor::Anchor),*
-        }
-    };
-    let impl_lookup: TokenStream = quote! {
-        impl #base_ident::application::gleis::anchor::Lookup<#enum_name> for #struct_name {
-            fn get(&self, key: #enum_name) -> &#base_ident::application::gleis::anchor::Anchor {
-                match key {
-                    #(#enum_name::#enum_variants => &self.#struct_fields),*
-                }
-            }
-            fn get_mut(&mut self, key: #enum_name) -> &mut #base_ident::application::gleis::anchor::Anchor {
-                match key {
-                    #(#enum_name::#enum_variants => &mut self.#struct_fields),*
-                }
-            }
-            fn for_each<F: FnMut(#enum_name, &#base_ident::application::gleis::anchor::Anchor)>(&self, mut action: F) {
-                #(action(#enum_name::#enum_variants, &self.#struct_fields));*
-            }
-            fn map<F: Fn(&#base_ident::application::gleis::anchor::Anchor)->#base_ident::application::gleis::anchor::Anchor>(&self, mut action: F) -> Self {
-                #struct_name {
-                    #(#struct_fields: action(&self.#struct_fields)),*
-                }
-            }
-        }
-    };
-    quote! {
-        #struct_definition
-        #impl_lookup
-    }
-}
-
 pub fn impl_lookup(args: Vec<syn::NestedMeta>, item: syn::ItemEnum) -> TokenStream {
     let mut errors = Vec::new();
 
@@ -97,8 +31,7 @@ pub fn impl_lookup(args: Vec<syn::NestedMeta>, item: syn::ItemEnum) -> TokenStre
         let syn::ItemEnum { vis, variants, ident, .. } = &item;
         let enum_variants: Vec<syn::Ident> = variants.iter().map(|v| v.ident.clone()).collect();
 
-        // construct a struct using a snake_case field for every variant, each holding an
-        // anchor::Anchor
+        // construct a struct using a snake_case field for every variant, each holding an Element
         let struct_name: syn::Ident =
             format_ident!("{}Elements", ident.to_string().trim_end_matches("Name"));
         let struct_fields: Vec<syn::Ident> = enum_variants
@@ -111,7 +44,7 @@ pub fn impl_lookup(args: Vec<syn::NestedMeta>, item: syn::ItemEnum) -> TokenStre
         struct_definition = Some(quote! {
             #[derive(Debug)]
             #vis struct #struct_name {
-                #(pub #struct_fields : #base_ident::application::gleis::anchor::Anchor),*
+                #(pub #struct_fields : #element),*
             }
         });
         impl_lookup = Some(quote! {
