1ba58912f7ba9f187ab24b89242d0034967e1438
spamviech <spamviech@web.de>
2021-06-07 00:32:43 +0200

    speichere Trigger in Kontakt

--- src/anschluss/trigger.rs
+++ src/anschluss/trigger.rs
@@ -3,28 +3,35 @@
 #[cfg(not(raspi))]
 use std::fmt::{Display, Formatter, Result};
 
-use cfg_if::cfg_if;
+use serde::{Deserialize, Serialize};
 
-cfg_if! {
-    if #[cfg(raspi)] {
-        pub use rppal::gpio::Trigger;
-    } else {
-        #[derive(Clone, Copy, Debug, PartialEq, Eq)]
-        pub enum Trigger {
-            Disabled,
-            RisingEdge,
-            FallingEdge,
-            Both,
-        }
-        impl Display for Trigger {
-            fn fmt(&self, f: &mut Formatter<'_>) -> Result {
-                write!(f, "{}", match self {
-                    Trigger::Disabled => "Disabled",
-                    Trigger::RisingEdge => "Rising Edge",
-                    Trigger::FallingEdge => "Falling Edge",
-                    Trigger::Both => "Both",
-                })
-            }
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Serialize, Deserialize)]
+pub enum Trigger {
+    Disabled,
+    RisingEdge,
+    FallingEdge,
+    Both,
+}
+
+impl Display for Trigger {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+        write!(f, "{}", match self {
+            Trigger::Disabled => "Disabled",
+            Trigger::RisingEdge => "Rising Edge",
+            Trigger::FallingEdge => "Falling Edge",
+            Trigger::Both => "Both",
+        })
+    }
+}
+
+#[cfg(raspi)]
+impl From<Trigger> for rppal::gpio::Trigger {
+    fn from(trigger: Trigger) -> rppal::gpio::Trigger {
+        match trigger {
+            Trigger::Disabled => rppal::gpio::Trigger::Disabled,
+            Trigger::RisingEdge => rppal::gpio::Trigger::RisingEdge,
+            Trigger::FallingEdge => rppal::gpio::Trigger::FallingEdge,
+            Trigger::Both => rppal::gpio::Trigger::Both,
         }
     }
 }
--- src/steuerung/kontakt.rs
+++ src/steuerung/kontakt.rs
@@ -18,6 +18,7 @@ use crate::anschluss::{
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Kontakt<Anschluss> {
     pub anschluss: Anschluss,
+    pub trigger: Trigger,
 }
 
 impl Kontakt<InputAnschluss> {
@@ -25,13 +26,11 @@ impl Kontakt<InputAnschluss> {
         self.anschluss.read()
     }
 
-    // TODO trigger in Kontakt speichern?
     pub fn set_async_interrupt(
         &mut self,
-        trigger: Trigger,
         callback: impl FnMut(Level) + Send + 'static,
     ) -> Result<(), Error> {
-        self.anschluss.set_async_interrupt(trigger, callback)
+        self.anschluss.set_async_interrupt(self.trigger, callback)
     }
 
     pub fn clear_async_interrupt(&mut self) -> Result<(), Error> {
@@ -41,13 +40,13 @@ impl Kontakt<InputAnschluss> {
 
 impl ToSave<Kontakt<InputSave>> for Kontakt<InputAnschluss> {
     fn to_save(&self) -> Kontakt<InputSave> {
-        Kontakt { anschluss: self.anschluss.to_save() }
+        Kontakt { anschluss: self.anschluss.to_save(), trigger: self.trigger }
     }
 }
 
 impl Reserviere<Kontakt<InputAnschluss>> for Kontakt<InputSave> {
     fn reserviere(self, anschlüsse: &mut Anschlüsse) -> Result<Kontakt<InputAnschluss>, Error> {
-        Ok(Kontakt { anschluss: self.anschluss.reserviere(anschlüsse)? })
+        Ok(Kontakt { anschluss: self.anschluss.reserviere(anschlüsse)?, trigger: self.trigger })
     }
 }
 
