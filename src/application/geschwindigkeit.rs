58e418ecbc0e0d6e984f8b19ec5bec31ebe3932d
spamviech <spamviech@web.de>
2021-06-09 16:12:28 +0200

    allgemeine neu_mit_leiter-Funktion

--- src/application/geschwindigkeit.rs
+++ src/application/geschwindigkeit.rs
@@ -13,6 +13,7 @@ use iced_native::{
     container,
     event,
     mouse,
+    overlay,
     radio,
     row,
     scrollable,
@@ -81,6 +82,60 @@ pub struct Anzeige<'t, Leiter, FahrtrichtungMarker, M, R> {
     aktuelle_fahrtrichtung: &'t mut FahrtrichtungMarker,
     row: Row<'t, M, R>,
 }
+impl<'t, Leiter, FahrtrichtungMarker, M, R> Anzeige<'t, Leiter, FahrtrichtungMarker, M, R>
+where
+    M: 'static + Clone,
+    R: 't + column::Renderer + row::Renderer + text::Renderer + slider::Renderer + radio::Renderer,
+{
+    pub fn neu_mit_leiter<S, Iter: Iterator>(
+        name: &'t Name,
+        geschwindigkeit: &'t Geschwindigkeit<Leiter>,
+        status: &'t mut AnzeigeStatus<S>,
+        ks_iter: impl FnOnce(&'t Geschwindigkeit<Leiter>) -> Option<Iter>,
+        geschwindigkeits_nachricht: impl Fn(u8) -> M + Clone + 'static,
+        zeige_fahrtrichtung: impl FnOnce(&'t mut S) -> (Element<'t, M, R>, &'t mut FahrtrichtungMarker),
+        // TODO overlay mit Anschlüssen?
+    ) -> Self {
+        let AnzeigeStatus { aktuelle_geschwindigkeit, pwm_slider_state, fahrtrichtung_state } =
+            status;
+        // TODO Anschluss-Anzeige (Expander über Overlay?)
+        let mut row = Row::new().push(Text::new(&name.0));
+        row = if let Some(iter) = ks_iter(geschwindigkeit) {
+            row.push(Column::with_children(
+                iter::once(())
+                    .chain(iter.map(|_| ()))
+                    .enumerate()
+                    .map(|(i, ())| {
+                        let i_u8 = i as u8;
+                        Radio::new(
+                            i_u8,
+                            i_u8.to_string(),
+                            Some(*aktuelle_geschwindigkeit),
+                            geschwindigkeits_nachricht.clone(),
+                        )
+                        .into()
+                    })
+                    .collect(),
+            ))
+        } else {
+            row.push(Slider::new(
+                pwm_slider_state,
+                0 ..= u8::MAX,
+                *aktuelle_geschwindigkeit,
+                geschwindigkeits_nachricht,
+            ))
+        };
+        let (fahrtrichtung_steuerung, fahrtrichtung_status) =
+            zeige_fahrtrichtung(fahrtrichtung_state);
+        row = row.push(fahrtrichtung_steuerung);
+        Anzeige {
+            geschwindigkeit,
+            aktuelle_geschwindigkeit,
+            aktuelle_fahrtrichtung: fahrtrichtung_status,
+            row,
+        }
+    }
+}
 
 #[derive(Debug, Clone)]
 pub enum MessageMittelleiter {
@@ -102,45 +157,26 @@ where
         geschwindigkeit: &'t Geschwindigkeit<Mittelleiter>,
         status: &'t mut AnzeigeStatus<(button::State, ())>,
     ) -> Self {
-        let AnzeigeStatus {
-            aktuelle_geschwindigkeit,
-            pwm_slider_state,
-            fahrtrichtung_state: (umdrehen_state, unit),
-        } = status;
-        // TODO Anschluss-Anzeige (Expander über Overlay?)
-        let mut row = match &geschwindigkeit.leiter {
-            Mittelleiter::Pwm { pin: _, polarität: _ } => {
-                Row::new().push(Text::new(&name.0)).push(Slider::new(
-                    pwm_slider_state,
-                    0 ..= u8::MAX,
-                    *aktuelle_geschwindigkeit,
-                    MessageMittelleiter::Geschwindigkeit,
-                ))
-            },
-            Mittelleiter::KonstanteSpannung { geschwindigkeit, letzter_wert: _, umdrehen: _ } => {
-                Row::new().push(Column::with_children(
-                    iter::once(())
-                        .chain(geschwindigkeit.iter().map(|_| ()))
-                        .enumerate()
-                        .map(|(i, ())| {
-                            let i_u8 = i as u8;
-                            Radio::new(
-                                i_u8,
-                                i_u8.to_string(),
-                                Some(*aktuelle_geschwindigkeit),
-                                MessageMittelleiter::Geschwindigkeit,
-                            )
-                            .into()
-                        })
-                        .collect(),
-                ))
-            },
+        let ks_iter = |Geschwindigkeit { leiter }: &'t Geschwindigkeit<Mittelleiter>| match leiter {
+            Mittelleiter::Pwm { .. } => None,
+            Mittelleiter::KonstanteSpannung { geschwindigkeit, .. } => Some(geschwindigkeit.iter()),
         };
-        row = row.push(
-            Button::new(umdrehen_state, Text::new("Umdrehen"))
-                .on_press(MessageMittelleiter::Umdrehen),
-        );
-        Anzeige { geschwindigkeit, aktuelle_geschwindigkeit, aktuelle_fahrtrichtung: unit, row }
+        let zeige_fahrtrichtung = |(button_state, unit): &'t mut (button::State, ())| {
+            (
+                Button::new(button_state, Text::new("Umdrehen"))
+                    .on_press(MessageMittelleiter::Umdrehen)
+                    .into(),
+                unit,
+            )
+        };
+        Anzeige::neu_mit_leiter(
+            name,
+            geschwindigkeit,
+            status,
+            ks_iter,
+            MessageMittelleiter::Geschwindigkeit,
+            zeige_fahrtrichtung,
+        )
     }
 }
 
@@ -158,57 +194,35 @@ where
         geschwindigkeit: &'t Geschwindigkeit<Zweileiter>,
         status: &'t mut AnzeigeStatus<Fahrtrichtung>,
     ) -> Self {
-        let AnzeigeStatus { aktuelle_geschwindigkeit, pwm_slider_state, fahrtrichtung_state } =
-            status;
-        // TODO Anschluss-Anzeige (Expander über Overlay?)
-        let mut row = Row::new().push(Text::new(&name.0));
-        row = match &geschwindigkeit.leiter {
-            Zweileiter::Pwm { geschwindigkeit: _, polarität: _, fahrtrichtung: _ } => {
-                row.push(Slider::new(
-                    pwm_slider_state,
-                    0 ..= u8::MAX,
-                    *aktuelle_geschwindigkeit,
-                    MessageZweileiter::Geschwindigkeit,
-                ))
-            },
-            Zweileiter::KonstanteSpannung {
-                geschwindigkeit,
-                letzter_wert: _,
-                fahrtrichtung: _,
-            } => row.push(Column::with_children(
-                iter::once(())
-                    .chain(geschwindigkeit.iter().map(|_| ()))
-                    .enumerate()
-                    .map(|(i, ())| {
-                        let i_u8 = i as u8;
-                        Radio::new(
-                            i_u8,
-                            i_u8.to_string(),
-                            Some(*aktuelle_geschwindigkeit),
-                            MessageZweileiter::Geschwindigkeit,
-                        )
-                        .into()
-                    })
-                    .collect(),
-            )),
+        let ks_iter = |Geschwindigkeit { leiter }: &'t Geschwindigkeit<Zweileiter>| match leiter {
+            Zweileiter::Pwm { .. } => None,
+            Zweileiter::KonstanteSpannung { geschwindigkeit, .. } => Some(geschwindigkeit.iter()),
         };
-        let fahrtrichtung_radio = |fahrtrichtung: Fahrtrichtung| {
+        let fahrtrichtung_radio = |fahrtrichtung: Fahrtrichtung, aktuell: &Fahrtrichtung| {
             Radio::new(
                 fahrtrichtung,
                 fahrtrichtung.to_string(),
-                Some(*fahrtrichtung_state),
+                Some(*aktuell),
                 MessageZweileiter::Fahrtrichtung,
             )
         };
-        row = row
-            .push(fahrtrichtung_radio(Fahrtrichtung::Vorwärts))
-            .push(fahrtrichtung_radio(Fahrtrichtung::Rückwärts));
-        Anzeige {
+        let zeige_fahrtrichtung = |fahrtrichtung: &'t mut Fahrtrichtung| {
+            (
+                Row::new()
+                    .push(fahrtrichtung_radio(Fahrtrichtung::Vorwärts, fahrtrichtung))
+                    .push(fahrtrichtung_radio(Fahrtrichtung::Rückwärts, fahrtrichtung))
+                    .into(),
+                fahrtrichtung,
+            )
+        };
+        Anzeige::neu_mit_leiter(
+            name,
             geschwindigkeit,
-            aktuelle_geschwindigkeit,
-            aktuelle_fahrtrichtung: fahrtrichtung_state,
-            row,
-        }
+            status,
+            ks_iter,
+            MessageZweileiter::Geschwindigkeit,
+            zeige_fahrtrichtung,
+        )
     }
 }
 
@@ -227,6 +241,10 @@ where
         _clipboard: &mut dyn Clipboard,
         _messages: &mut Vec<M>,
     ) -> event::Status {
-        todo!()
+        todo!("on_event")
+    }
+
+    fn overlay(&mut self, _layout: Layout<'_>) -> Option<overlay::Element<'_, M, R>> {
+        todo!("overlay")
     }
 }
