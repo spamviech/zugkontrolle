a5b62553cfcd34be404303b1cc09352687643157
spamviech <spamviech@web.de>
2021-09-12 17:44:02 +0200

    derive(Debug)

--- src/steuerung/plan.rs
+++ src/steuerung/plan.rs
@@ -5,5 +5,5 @@ use serde::{Deserialize, Serialize};
 /// TODO Dummy-Typ; wenn fertig wird er einen automatischen Fahrplan darstellen.
 /// Die erste Anpassung in einen sinnvollen Wert (wahrscheinlich ein Record struct)
 /// wird die SemVer nicht zwingend berücksichtigen (kann ohne erhöhen der major version erfolgen).
-#[derive(Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize)]
 pub struct Plan;
