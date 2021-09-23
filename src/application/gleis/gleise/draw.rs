b734d1624add730302665108f7ba329d8de94820
spamviech <spamviech@web.de>
2021-09-23 16:24:36 +0200

    typ-Fehler durch todo!-Macros ersetzt.

--- src/application/gleis/gleise/draw.rs
+++ src/application/gleis/gleise/draw.rs
@@ -1,5 +1,7 @@
 //! draw-Methode für Gleise
 
+use rstar::primitives::Rectangle;
+
 use crate::{
     anschluss::Fließend,
     application::{
@@ -20,22 +22,21 @@ pub(crate) fn move_to_position(frame: &mut canvas::Frame, position: &Position) {
     frame.transformation(&Transformation::Rotation(position.winkel));
 }
 
-fn fülle_alle_gleise<T, Z>(
+fn fülle_alle_gleise<'t, T: Zeichnen>(
     frame: &mut canvas::Frame,
-    map: &RStern<T>,
-    transparent: impl Fn(AnyId<Z>, Fließend) -> Transparenz,
+    rstern: &'t RStern<T>,
+    transparent: impl Fn(&'t Rectangle<Vektor>, Fließend) -> Transparenz,
     streckenabschnitt_farbe: &Farbe,
     streckenabschnitt_fließend: &Fließend,
-) where
-    T: Zeichnen,
-    GleisId<T>: Into<AnyId<Z>>,
-{
-    for (gleis_id, Gleis { definition, position, .. }) in map.iter() {
+) {
+    for geom_with_data in rstern.iter() {
+        let rectangle = geom_with_data.geom();
+        let (definition, position) = &geom_with_data.data;
         frame.with_save(|frame| {
             move_to_position(frame, position);
             // einfärben
             for (path, transparenz) in definition.fülle() {
-                let a = transparent(AnyId::from_ref(gleis_id), *streckenabschnitt_fließend)
+                let a = transparent(rectangle, *streckenabschnitt_fließend)
                     .kombiniere(transparenz)
                     .alpha();
                 frame.with_save(|frame| {
@@ -48,22 +49,20 @@ fn fülle_alle_gleise<T, Z>(
     }
 }
 
-fn zeichne_alle_gleise<T, Z>(
+fn zeichne_alle_gleise<'t, T: Zeichnen>(
     frame: &mut canvas::Frame,
-    map: &RStern<T>,
-    is_grabbed: impl Fn(AnyId<Z>) -> bool,
-) where
-    T: Zeichnen,
-    GleisId<T>: Into<AnyId<Z>>,
-{
-    for (gleis_id, Gleis { definition, position, .. }) in map.iter() {
+    rstern: &'t RStern<T>,
+    ist_gehalten: impl Fn(&'t Rectangle<Vektor>) -> bool,
+) {
+    for geom_with_data in rstern.iter() {
+        let rectangle = geom_with_data.geom();
+        let (definition, position) = &geom_with_data.data;
         frame.with_save(|frame| {
             move_to_position(frame, position);
             // zeichne Kontur
             for path in definition.zeichne() {
                 frame.with_save(|frame| {
-                    let a =
-                        Transparenz::true_reduziert(is_grabbed(AnyId::from_ref(gleis_id))).alpha();
+                    let a = Transparenz::true_reduziert(ist_gehalten(rectangle)).alpha();
                     frame.stroke(
                         &path,
                         canvas::Stroke {
@@ -78,39 +77,42 @@ fn zeichne_alle_gleise<T, Z>(
     }
 }
 
-fn zeichne_alle_anchor_points<T, Z>(
+fn zeichne_alle_anchor_points<'r, 's, 't: 'r + 's, T: Zeichnen>(
     frame: &mut canvas::Frame,
-    map: &RStern<T>,
-    has_other_and_grabbed_id_at_point: impl Fn(AnyId<Z>, verbindung::Verbindung) -> (bool, bool),
-    is_grabbed: impl Fn(AnyId<Z>) -> bool,
-) where
-    T: Zeichnen,
-    GleisId<T>: Into<AnyId<Z>>,
-{
-    for (gleis_id, Gleis { definition, position, .. }) in map.iter() {
+    rstern: &'t RStern<T>,
+    existiert_andere_oder_gehaltene_verbindung: impl Fn(
+        &'r Rectangle<Vektor>,
+        &'r verbindung::Verbindung,
+    ) -> (bool, bool),
+    ist_gehalten: impl Fn(&'s Rectangle<Vektor>) -> bool,
+) {
+    for geom_with_data in rstern.iter() {
+        let rectangle = geom_with_data.geom();
+        let (definition, position) = &geom_with_data.data;
         frame.with_save(|frame| {
             move_to_position(frame, position);
             // zeichne anchor points
-            definition.anchor_points().for_each(|_name, &anchor| {
+            definition.verbindungen().for_each(|_name, &verbindung| {
+                let verbindung_an_position = verbindung::Verbindung {
+                    position: position.transformation(verbindung.position),
+                    richtung: position.winkel + verbindung.richtung,
+                };
                 frame.with_save(|frame| {
-                    let (opposing, grabbed) = has_other_and_grabbed_id_at_point(
-                        AnyId::from_ref(gleis_id),
-                        verbindung::Verbindung {
-                            position: position.transformation(anchor.position),
-                            richtung: position.winkel + anchor.richtung,
-                        },
+                    let (opposing, grabbed) = existiert_andere_oder_gehaltene_verbindung(
+                        rectangle,
+                        &verbindung_an_position,
                     );
-                    let a =
-                        Transparenz::true_reduziert(is_grabbed(AnyId::from_ref(gleis_id))).alpha();
+                    let a = Transparenz::true_reduziert(ist_gehalten(rectangle)).alpha();
                     let g = if opposing { 1. } else { 0. };
                     let color = canvas::Color { r: 0., g, b: 1. - g, a };
-                    let direction: Vektor = Vektor::polar_koordinaten(Skalar(5.), anchor.richtung);
+                    let direction: Vektor =
+                        Vektor::polar_koordinaten(Skalar(5.), verbindung.richtung);
                     let direction_side: Vektor = Skalar(0.5) * direction.rotiert(winkel::FRAC_PI_2);
-                    let anchor_position: Vektor = anchor.position;
+                    let verbindung_position: Vektor = verbindung.position;
                     let mut path_builder = pfad::Erbauer::neu();
-                    path_builder.move_to(anchor_position + direction_side);
-                    path_builder.line_to(anchor_position + direction);
-                    path_builder.line_to(anchor_position - direction_side);
+                    path_builder.move_to(verbindung_position + direction_side);
+                    path_builder.line_to(verbindung_position + direction);
+                    path_builder.line_to(verbindung_position - direction_side);
                     let path = path_builder.baue();
                     frame.stroke(&path, canvas::Stroke { color, width: 1.5, ..Default::default() });
                     // fill on connect/snap for drag&drop
@@ -123,15 +125,14 @@ fn zeichne_alle_anchor_points<T, Z>(
     }
 }
 
-fn schreibe_alle_beschreibungen<T, Z>(
+fn schreibe_alle_beschreibungen<'t, T: Zeichnen>(
     frame: &mut canvas::Frame,
-    map: &RStern<T>,
-    is_grabbed: impl Fn(AnyId<Z>) -> bool,
-) where
-    T: Zeichnen,
-    GleisId<T>: Into<AnyId<Z>>,
-{
-    for (gleis_id, Gleis { definition, position, .. }) in map.iter() {
+    rstern: &'t RStern<T>,
+    ist_gehalten: impl Fn(&'t Rectangle<Vektor>) -> bool,
+) {
+    for geom_with_data in rstern.iter() {
+        let rectangle = geom_with_data.geom();
+        let (definition, position) = &geom_with_data.data;
         let (relative_position, beschreibung, name) = definition.beschreibung_und_name();
         if let Some(content) = match (beschreibung, name) {
             (Some(beschreibung), Some(name)) => Some(format!("{} ({})", name, beschreibung)),
@@ -145,7 +146,7 @@ fn schreibe_alle_beschreibungen<T, Z>(
             let absolute_position = Position { punkt, winkel };
             frame.with_save(|frame| {
                 move_to_position(frame, &absolute_position);
-                let a = Transparenz::true_reduziert(is_grabbed(AnyId::from_ref(gleis_id))).alpha();
+                let a = Transparenz::true_reduziert(ist_gehalten(rectangle)).alpha();
                 frame.fill_text(canvas::Text {
                     content,
                     position: iced::Point::ORIGIN,
@@ -165,7 +166,7 @@ impl<Z: Zugtyp> Gleise<Z> {
         bounds: iced::Rectangle,
         _cursor: iced::canvas::Cursor,
     ) -> Vec<iced::canvas::Geometry> {
-        let Gleise { canvas, zustand, verbindungen, modus, .. } = self;
+        let Gleise { canvas, zustand, modus, .. } = self;
         vec![canvas.zeichnen_skaliert_von_pivot(
             bounds.size(),
             &self.pivot,
@@ -173,30 +174,34 @@ impl<Z: Zugtyp> Gleise<Z> {
             |frame| {
                 // TODO zeichne keine out-of-bounds Gleise
                 // Zeichne Gleise
-                let grabbed_id: Option<AnyId<Z>>;
+                let gehalten_id: Option<AnyId<Z>>;
                 let modus_bauen: bool;
                 match modus {
                     ModusDaten::Bauen { gehalten: Some(Gehalten { gleis_id, .. }), .. } => {
-                        grabbed_id = Some(gleis_id.clone());
+                        gehalten_id = Some(gleis_id.clone());
                         modus_bauen = true;
                     }
                     ModusDaten::Bauen { gehalten: None, .. } => {
-                        grabbed_id = None;
+                        gehalten_id = None;
                         modus_bauen = true;
                     }
                     ModusDaten::Fahren => {
-                        grabbed_id = None;
+                        gehalten_id = None;
                         modus_bauen = false;
                     }
                 };
-                // TODO markiere grabbed als "wird-gelöscht", falls cursor out of bounds ist
-                let is_grabbed = |parameter_id| Some(parameter_id) == grabbed_id;
-                let has_other_and_grabbed_id_at_point = |gleis_id, position| {
-                    verbindungen.hat_andere_id_und_grabbed_an_position(
-                        &gleis_id,
-                        |id| is_grabbed(id.clone()),
-                        &position,
-                    )
+                // TODO markiere gehalten als "wird-gelöscht", falls cursor out of bounds ist
+                let ist_gehalten = |parameter_id| {
+                    // Some(parameter_id) == grabbed_id
+                    todo!()
+                };
+                let existiert_andere_oder_gehaltene_verbindung = |gleis_id, position| {
+                    // verbindungen.hat_andere_id_und_grabbed_an_position(
+                    //     &gleis_id,
+                    //     |id| ist_gehalten(id.clone()),
+                    //     &position,
+                    // )
+                    todo!()
                 };
 
                 macro_rules! mit_allen_gleisen {
@@ -217,8 +222,8 @@ impl<Z: Zugtyp> Gleise<Z> {
                     mit_allen_gleisen! {
                         maps,
                         fülle_alle_gleise,
-                        |gleis_id, fließend| Transparenz::true_reduziert(if modus_bauen {
-                            is_grabbed(gleis_id)
+                        |rectangle, fließend| Transparenz::true_reduziert(if modus_bauen {
+                            ist_gehalten(rectangle)
                         } else {
                             fließend == Fließend::Gesperrt
                         }),
@@ -231,7 +236,7 @@ impl<Z: Zugtyp> Gleise<Z> {
                     mit_allen_gleisen! {
                         maps,
                         zeichne_alle_gleise,
-                        is_grabbed
+                        ist_gehalten
                     }
                 }
                 // Verbindungen
@@ -239,8 +244,8 @@ impl<Z: Zugtyp> Gleise<Z> {
                     mit_allen_gleisen! {
                         maps,
                         zeichne_alle_anchor_points,
-                        has_other_and_grabbed_id_at_point,
-                        &is_grabbed
+                        existiert_andere_oder_gehaltene_verbindung,
+                        &ist_gehalten
                     }
                 }
                 // Beschreibung
@@ -248,7 +253,7 @@ impl<Z: Zugtyp> Gleise<Z> {
                     mit_allen_gleisen! {
                         maps,
                         schreibe_alle_beschreibungen,
-                        is_grabbed
+                        ist_gehalten
                     }
                 }
             },
