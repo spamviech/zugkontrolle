b538e83ec60df51d5327307ae3f37524219b81b4
spamviech <spamviech@web.de>
2021-09-28 01:31:23 +0200

    verwede aktuell::Gleis::from für explizitere konvertierung

--- src/application/gleis/gleise/daten/v2.rs
+++ src/application/gleis/gleise/daten/v2.rs
@@ -3,11 +3,10 @@
 use serde::{Deserialize, Serialize};
 
 use crate::{
-    anschluss::de_serialisieren::Serialisiere,
     application::{
         gleis::{
             gerade::GeradeSerialisiert,
-            gleise::daten::{self as aktuell},
+            gleise::daten as aktuell,
             kreuzung::KreuzungSerialisiert,
             kurve::KurveSerialisiert,
             weiche::{
@@ -52,13 +51,13 @@ impl<Z: Zugtyp> From<GleiseVecs<Z>> for aktuell::Serialisiert<Z> {
     fn from(v2: GleiseVecs<Z>) -> Self {
         aktuell::Serialisiert {
             zugtyp: v2.name,
-            geraden: v2.geraden.into_iter().map(Into::into).collect(),
-            kurven: v2.kurven.into_iter().map(Into::into).collect(),
-            weichen: v2.weichen.into_iter().map(Into::into).collect(),
-            dreiwege_weichen: v2.dreiwege_weichen.into_iter().map(Into::into).collect(),
-            kurven_weichen: v2.kurven_weichen.into_iter().map(Into::into).collect(),
-            s_kurven_weichen: v2.s_kurven_weichen.into_iter().map(Into::into).collect(),
-            kreuzungen: v2.kreuzungen.into_iter().map(Into::into).collect(),
+            geraden: v2.geraden.into_iter().map(aktuell::Gleis::from).collect(),
+            kurven: v2.kurven.into_iter().map(aktuell::Gleis::from).collect(),
+            weichen: v2.weichen.into_iter().map(aktuell::Gleis::from).collect(),
+            dreiwege_weichen: v2.dreiwege_weichen.into_iter().map(aktuell::Gleis::from).collect(),
+            kurven_weichen: v2.kurven_weichen.into_iter().map(aktuell::Gleis::from).collect(),
+            s_kurven_weichen: v2.s_kurven_weichen.into_iter().map(aktuell::Gleis::from).collect(),
+            kreuzungen: v2.kreuzungen.into_iter().map(aktuell::Gleis::from).collect(),
             streckenabschnitte: v2.streckenabschnitte,
             geschwindigkeiten: v2.geschwindigkeiten,
             pläne: v2.pläne,
