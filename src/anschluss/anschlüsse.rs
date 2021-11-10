c720425661fc1916f9f52466bfa6e9c1040871df
spamviech <spamviech@web.de>
2021-11-10 11:55:06 +0100

    Anschlüsse existiert nur noch als Singleton

--- src/anschluss/anschlüsse.rs
+++ src/anschluss/anschlüsse.rs
@@ -3,8 +3,9 @@
 #[cfg(not(raspi))]
 use std::collections::HashSet;
 use std::sync::{
+    self,
     mpsc::{channel, Receiver, Sender},
-    Arc, Mutex, PoisonError,
+    Arc, Mutex, MutexGuard, PoisonError,
 };
 use std::thread;
 
@@ -40,7 +41,7 @@ macro_rules! anschlüsse_data {
         paste! {
             #[doc="Singleton für Zugriff auf raspberry pi Anschlüsse."]
             #[derive(Debug)]
-            struct AnschlüsseData {
+            pub struct Anschlüsse {
                 #[cfg(raspi)]
                 gpio: rppal::gpio::Gpio,
                 #[cfg(raspi)]
@@ -87,11 +88,17 @@ macro_rules! llln_to_hhha {
 }
 
 llln_to_hhha! { anschlüsse_data }
-impl AnschlüsseData {
-    /// Gebe den Pcf8574 an Anschlüsse zurück, so dass er von anderen verwendet werden kann.
+impl Anschlüsse {
+    /// Erhalte Zugriff auf das Singleton.
+    pub fn lock<'t>() -> sync::LockResult<MutexGuard<'t, Anschlüsse>> {
+        ANSCHLÜSSE.lock()
+    }
+
+    /// Gebe den Pcf8574-Port an Anschlüsse zurück, so dass er von anderen verwendet werden kann.
     ///
-    /// Wird vom Drop-handler ausgeführt, hier ist es explizit.
-    fn rückgabe(&mut self, port: Port) -> Result<(), SyncFehler> {
+    /// Der Drop-Handler von Pcf8574 (und dem letzten Pcf8574Port) hat die selbe Auswirkung.
+    /// Diese Methode ist explizit (keine Wartezeit, kann dafür blockieren).
+    pub fn rückgabe(&mut self, port: Port) -> Result<(), SyncFehler> {
         macro_rules! match_pcf8574 {
             {$($k:ident $l:ident $m:ident $n:ident),*} => {
                 paste! {
@@ -128,7 +135,7 @@ impl AnschlüsseData {
     }
 
     /// Reserviere den spezifizierten Pcf8574 zur exklusiven Nutzung.
-    fn reserviere_pcf8574_port(
+    pub fn reserviere_pcf8574_port(
         &mut self,
         a0: Level,
         a1: Level,
@@ -164,43 +171,38 @@ impl AnschlüsseData {
         }
         llln_to_hhha! {reserviere_pcf8574}
     }
-}
 
-/// Singleton für Zugriff auf raspberry pi Anschlüsse.
-#[derive(Debug)]
-pub struct Anschlüsse(Option<AnschlüsseInternal>);
-impl Drop for Anschlüsse {
-    fn drop(&mut self) {
-        let Anschlüsse(option_data) = self;
-        if let Ok(mut guard) = ANSCHLÜSSE.lock() {
-            let static_anschlüsse = &mut *guard;
-            if let Err(Fehler::Sync(SyncFehler::SingletonInVerwendung)) = static_anschlüsse {
-                if let Some(anschlüsse) = std::mem::replace(option_data, None) {
-                    *static_anschlüsse = Ok(anschlüsse);
-                } else {
-                    error!("None-Wert in Anschlüsse während drop!",);
-                }
+    /// Reserviere den spezifizierten Pin zur exklusiven Nutzung.
+    pub fn reserviere_pin(&mut self, pin: u8) -> Result<Pin, Fehler> {
+        debug!("reserviere pin {}", pin);
+        if let 2 | 3 = pin {
+            // Gpio 2,3 nicht verfügbar (durch I2C belegt)
+            return Err(Fehler::Sync(SyncFehler::AnschlussInVerwendung(
+                AnschlussBeschreibung::Pin(pin),
+            )));
+        }
+        #[cfg(raspi)]
+        {
+            Ok(Pin::neu(self.gpio.get(pin)?))
+        }
+        #[cfg(not(raspi))]
+        {
+            if self.ausgegebene_pins.insert(pin) {
+                Ok(Pin::neu(pin, self.pin_rückgabe.clone()))
+            } else {
+                Err(Fehler::Sync(SyncFehler::AnschlussInVerwendung(AnschlussBeschreibung::Pin(
+                    pin,
+                ))))
             }
         }
     }
-}
-impl Anschlüsse {
-    pub fn neu() -> Result<Anschlüsse, Fehler> {
-        let mut guard = ANSCHLÜSSE.lock()?;
-        let anschlüsse = &mut *guard;
-        let data =
-            std::mem::replace(anschlüsse, Err(Fehler::Sync(SyncFehler::SingletonInVerwendung)))?;
-        Ok(Anschlüsse(Some(data)))
-    }
-
     fn listen_restore_messages(
         sender: Sender<(pcf8574::Beschreibung, u3)>,
         receiver: Receiver<(pcf8574::Beschreibung, u3)>,
-        inner: AnschlüsseInternal,
     ) {
         loop {
             match receiver.recv() {
-                Ok((nachricht, port)) => match inner.lock() {
+                Ok((nachricht, port)) => match ANSCHLÜSSE.lock() {
                     Ok(mut guard) => {
                         let anschlüsse = &mut *guard;
                         macro_rules! port_value {
@@ -258,10 +260,10 @@ impl Anschlüsse {
     }
 
     #[cfg(not(raspi))]
-    fn listen_pin_restore_messages(receiver: Receiver<u8>, inner: AnschlüsseInternal) {
+    fn listen_pin_restore_messages(receiver: Receiver<u8>) {
         loop {
             match receiver.recv() {
-                Ok(pin) => match inner.lock() {
+                Ok(pin) => match ANSCHLÜSSE.lock() {
                     Ok(mut guard) => {
                         let anschlüsse = &mut *guard;
                         debug!("rückgabe pin {}", pin);
@@ -280,11 +282,15 @@ impl Anschlüsse {
         }
     }
 
-    fn erstelle_static() -> AnschlüsseStatic {
+    fn erstelle_static() -> Mutex<Anschlüsse> {
         #[cfg(not(raspi))]
         let (pin_sender, pin_receiver) = channel();
+
+        let (sender, receiver) = channel();
+        let sender_clone = sender.clone();
+
         macro_rules! make_anschlüsse {
-            {$($a0:ident $a1:ident $a2:ident $var:ident),*} => {{
+            { $($a0:ident $a1:ident $a2:ident $var:ident),* } => {{
                 #[cfg(raspi)]
                 let i2c = match rppal::i2c::I2c::new() {
                     Ok(i2c) => Arc::new(Mutex::new(i2c)),
@@ -296,7 +302,38 @@ impl Anschlüsse {
                     Err(error) => return Arc::new(Mutex::new(Err(error.into()))),
                 };
                 paste! {
-                    Ok(AnschlüsseData {
+                    $(
+                        let [<$a0 $a1 $a2 $var>] = Arc::new(Mutex::new(Pcf8574::neu(
+                            level!($a0),
+                            level!($a1),
+                            level!($a2),
+                            variante!($var),
+                            #[cfg(raspi)]
+                            i2c.clone(),
+                        )));
+                    )*
+                }
+                macro_rules! port_value {
+                    ($v_a0:ident, $v_a1:ident, $v_a2:ident, $v_var:ident, $v_port:expr) => {
+                        paste! {
+                            Some(
+                                Port::neu(
+                                    [<$v_a0 $v_a1 $v_a2 $v_var>].clone(),
+                                    pcf8574::Beschreibung {
+                                        a0:level!{$v_a0},
+                                        a1:level!{$v_a1},
+                                        a2:level!{$v_a2},
+                                        variante:variante!{$v_var}
+                                    },
+                                    u3::new($v_port),
+                                    sender.clone()
+                                )
+                            )
+                        }
+                    };
+                }
+                paste! {
+                    Anschlüsse {
                         #[cfg(raspi)]
                         gpio,
                         #[cfg(raspi)]
@@ -306,172 +343,39 @@ impl Anschlüsse {
                         #[cfg(not(raspi))]
                         pin_rückgabe: pin_sender,
                         $(
-                            [<$a0 $a1 $a2 $var>]: Arc::new(Mutex::new(Pcf8574::neu(
-                                level!($a0),
-                                level!($a1),
-                                level!($a2),
-                                variante!($var),
-                                #[cfg(raspi)]
-                                i2c.clone(),
-                            ))),
-                            [<$a0 $a1 $a2 $var 0>]: None,
-                            [<$a0 $a1 $a2 $var 1>]: None,
-                            [<$a0 $a1 $a2 $var 2>]: None,
-                            [<$a0 $a1 $a2 $var 3>]: None,
-                            [<$a0 $a1 $a2 $var 4>]: None,
-                            [<$a0 $a1 $a2 $var 5>]: None,
-                            [<$a0 $a1 $a2 $var 6>]: None,
-                            [<$a0 $a1 $a2 $var 7>]: None,
+                            [<$a0 $a1 $a2 $var>],
+                            [<$a0 $a1 $a2 $var 0>]: port_value!($a0, $a1, $a2, $var, 0),
+                            [<$a0 $a1 $a2 $var 1>]: port_value!($a0, $a1, $a2, $var, 1),
+                            [<$a0 $a1 $a2 $var 2>]: port_value!($a0, $a1, $a2, $var, 2),
+                            [<$a0 $a1 $a2 $var 3>]: port_value!($a0, $a1, $a2, $var, 3),
+                            [<$a0 $a1 $a2 $var 4>]: port_value!($a0, $a1, $a2, $var, 4),
+                            [<$a0 $a1 $a2 $var 5>]: port_value!($a0, $a1, $a2, $var, 5),
+                            [<$a0 $a1 $a2 $var 6>]: port_value!($a0, $a1, $a2, $var, 6),
+                            [<$a0 $a1 $a2 $var 7>]: port_value!($a0, $a1, $a2, $var, 7),
                         )*
-                    })
+                    }
                 }
             }};
         }
-        let inner = (llln_to_hhha! {make_anschlüsse}).map(|anschlüsse| {
-            let inner = Arc::new(Mutex::new(anschlüsse));
-            let inner_clone = inner.clone();
 
-            let (sender, receiver) = channel();
-            let sender_clone = sender.clone();
+        let anschlüsse = llln_to_hhha! {make_anschlüsse};
 
-            // erzeuge Thread der Rückgaben behandelt
-            let _ = thread::spawn(move || {
-                Anschlüsse::listen_restore_messages(sender_clone, receiver, inner_clone)
-            });
+        // erzeuge Thread der Rückgaben behandelt
+        let _ = thread::spawn(move || Anschlüsse::listen_restore_messages(sender_clone, receiver));
 
-            #[cfg(not(raspi))]
-            {
-                let inner_clone = inner.clone();
-
-                // erzeuge Thread der Pin-Rückgaben behandelt
-                let _ = thread::spawn(move || {
-                    Anschlüsse::listen_pin_restore_messages(pin_receiver, inner_clone)
-                });
-            }
-
-            // Eigener Block um borrow-lifetime von inner zu beschränken
-            {
-                let anschlüsse =
-                    &mut *inner.lock().expect("Anschlüsse poisoned vor Initialisierung");
-                macro_rules! port_value {
-                    ($a0:ident $a1:ident $a2:ident $var:ident $port:expr) => {
-                        paste! {
-                            Some(
-                                Port::neu(
-                                    anschlüsse.[<$a0 $a1 $a2 $var>].clone(),
-                                    pcf8574::Beschreibung {
-                                        a0:level!{$a0},
-                                        a1:level!{$a1},
-                                        a2:level!{$a2},
-                                        variante:variante!{$var}
-                                    },
-                                    u3::new($port),
-                                    sender.clone()
-                                )
-                            )
-                        }
-                    };
-                }
-                macro_rules! init_anschlüsse {
-                    {$($k:ident $l:ident $m:ident $n:ident),*: $value:ident} => {
-                        paste! {
-                            $(
-                                anschlüsse.[<$k $l $m $n 0>] = $value!($k $l $m $n 0);
-                                anschlüsse.[<$k $l $m $n 1>] = $value!($k $l $m $n 1);
-                                anschlüsse.[<$k $l $m $n 2>] = $value!($k $l $m $n 2);
-                                anschlüsse.[<$k $l $m $n 3>] = $value!($k $l $m $n 3);
-                                anschlüsse.[<$k $l $m $n 4>] = $value!($k $l $m $n 4);
-                                anschlüsse.[<$k $l $m $n 5>] = $value!($k $l $m $n 5);
-                                anschlüsse.[<$k $l $m $n 6>] = $value!($k $l $m $n 6);
-                                anschlüsse.[<$k $l $m $n 7>] = $value!($k $l $m $n 7);
-                            )*
-                        }
-                    };
-                }
-                llln_to_hhha! {init_anschlüsse: port_value}
-            }
-
-            inner
-        });
-
-        Arc::new(Mutex::new(inner))
-    }
-
-    /// Gebe den Pcf8574 an Anschlüsse zurück, so dass er von anderen verwendet werden kann.
-    ///
-    /// Der Drop-Handler von Pcf8574 (und dem letzten Pcf8574Port) hat die selbe Auswirkung.
-    /// Diese Methode ist explizit (keine Wartezeit, kann dafür blockieren).
-    pub fn rückgabe(&mut self, port: Port) -> Result<(), SyncFehler> {
-        if let Some(arc) = &self.0 {
-            arc.lock()?.rückgabe(port)
-        } else {
-            Err(SyncFehler::SingletonInVerwendung)
+        #[cfg(not(raspi))]
+        {
+            // erzeuge Thread der Pin-Rückgaben behandelt
+            let _ = thread::spawn(move || Anschlüsse::listen_pin_restore_messages(pin_receiver));
         }
-    }
 
-    /// Reserviere den spezifizierten Pin zur exklusiven Nutzung.
-    /// Rückgabe über den Drop-Handler (nur bei Raspi).
-    pub fn reserviere_pin(&mut self, pin: u8) -> Result<Pin, Fehler> {
-        debug!("reserviere pin {}", pin);
-        if let 2 | 3 = pin {
-            // Gpio 2,3 nicht verfügbar (durch I2C belegt)
-            return Err(Fehler::Sync(SyncFehler::AnschlussInVerwendung(
-                AnschlussBeschreibung::Pin(pin),
-            )));
-        }
-        if let Some(arc) = self.0.as_ref() {
-            #[cfg_attr(raspi, allow(unused_mut))]
-            let mut anschlüsse = arc.lock()?;
-            #[cfg(raspi)]
-            {
-                Ok(Pin::neu(anschlüsse.gpio.get(pin)?))
-            }
-            #[cfg(not(raspi))]
-            {
-                if anschlüsse.ausgegebene_pins.insert(pin) {
-                    Ok(Pin::neu(pin, anschlüsse.pin_rückgabe.clone()))
-                } else {
-                    Err(Fehler::Sync(SyncFehler::AnschlussInVerwendung(
-                        AnschlussBeschreibung::Pin(pin),
-                    )))
-                }
-            }
-        } else {
-            Err(Fehler::Sync(SyncFehler::SingletonInVerwendung))
-        }
-    }
-
-    /// Reserviere den spezifizierten Pcf8574-Port zur exklusiven Nutzung.
-    pub fn reserviere_pcf8574_port(
-        &mut self,
-        a0: Level,
-        a1: Level,
-        a2: Level,
-        variante: pcf8574::Variante,
-        port: u3,
-    ) -> Result<Port, SyncFehler> {
-        self.0.as_mut().ok_or(SyncFehler::WertDropped).and_then(|arc| {
-            // TODO besserer Fehler (welche Port wurde angefragt)
-            arc.lock().map_err(SyncFehler::from).and_then(|mut guard| {
-                guard.reserviere_pcf8574_port(a0, a1, a2, variante, port).ok_or(
-                    SyncFehler::AnschlussInVerwendung(AnschlussBeschreibung::Pcf8574Port {
-                        a0,
-                        a1,
-                        a2,
-                        variante,
-                        port,
-                    }),
-                )
-            })
-        })
+        Mutex::new(anschlüsse)
     }
 }
 
-type AnschlüsseInternal = Arc<Mutex<AnschlüsseData>>;
-type AnschlüsseResult = Result<AnschlüsseInternal, Fehler>;
-type AnschlüsseStatic = Arc<Mutex<AnschlüsseResult>>;
-static ANSCHLÜSSE: Lazy<AnschlüsseStatic> = Lazy::new(Anschlüsse::erstelle_static);
+static ANSCHLÜSSE: Lazy<Mutex<Anschlüsse>> = Lazy::new(Anschlüsse::erstelle_static);
 
+// FIXME nicht mehr benötigte Fehler-Fälle entfernen!
 #[allow(missing_copy_implementations)]
 #[derive(Debug)]
 pub enum Fehler {
