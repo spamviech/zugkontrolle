6ef25fad57ecf486f920dd6c87e68643184e7d8e
spamviech <spamviech@web.de>
2022-01-06 12:55:37 +0100

    once_cell ist immer dependency
    once_cell ist eh schon im Abhängigkeitsbaum
    nicht-raspi target-dependency ist nur seltsam zu formulieren

--- src/rppal.rs
+++ src/rppal.rs
@@ -1,5 +1,9 @@
-//! Mock-Methoden oder re-export für rppal
+//! Mock-Methoden oder re-export für rppal.
 
 pub mod gpio;
 pub mod i2c;
 pub mod pwm;
+
+#[cfg(raspi)]
+// Wird auf nicht-Raspi-Systemen für mock-Implementierung verwendet.
+use once_cell as _;
