563cd3595cbe8f1bf3fee5246b3b892fcca8d484
spamviech <spamviech@web.de>
2022-02-01 10:57:40 +0100

    verwende git-submodule für source-serif-Schriftart

--- src/application/fonts.rs
+++ src/application/fonts.rs
@@ -1,6 +1,6 @@
-//! Schriftarten der Anwendung (Adobe-Source unter OFL Lizenz)
+//! Schriftarten der Anwendung (Adobe-Source unter OFL Lizenz).
 
 use std::include_bytes;
 
-/// Schriftart ohne zusätzliche Eigenschaften
-pub static REGULAR: &[u8] = include_bytes!("../../fonts/SourceSerif4-Regular.ttf");
+/// Schriftart ohne zusätzliche Eigenschaften.
+pub static REGULAR: &[u8] = include_bytes!("../../fonts/source-serif/TTF/SourceSerif4-Regular.ttf");
