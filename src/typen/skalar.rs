b48bd55d491ef6ed67f08154b197bde5b1483db9
spamviech <spamviech@web.de>
2022-04-06 00:37:14 +0200

    docstring-anpassungen

--- src/typen/skalar.rs
+++ src/typen/skalar.rs
@@ -1,37 +1,34 @@
 //! Skalar-Werte für Größen auf dem Canvas.
 
-// HACK cargo check takes very long, this should reduce it until the lint is addressed
-#![allow(missing_docs)]
-
 use std::ops::{Add, AddAssign, Div, DivAssign, Mul, MulAssign, Neg, Sub, SubAssign};
 
 use serde::{Deserialize, Serialize};
 
 use crate::typen::mm;
 
-/// Skalar-Werte für Größen auf dem Canvas
+/// Skalar-Werte für Größen auf dem Canvas.
 ///
 /// Methoden bilden den bekannten arithmetischen Körper.
 #[derive(Debug, Clone, Copy, PartialEq, PartialOrd, Serialize, Deserialize)]
 pub struct Skalar(pub f32);
 
 impl Skalar {
-    /// Doppelter Wert
+    /// Doppelter Wert.
     pub fn doppelt(&self) -> Self {
         Skalar(2.) * self
     }
 
-    /// Halber Wert
+    /// Halber Wert.
     pub fn halbiert(&self) -> Self {
         Skalar(0.5) * self
     }
 
-    /// Absoluter Wert
+    /// Absoluter Wert.
     pub fn abs(&self) -> Self {
         Skalar(self.0.abs())
     }
 
-    /// Kopie des größeren Elements
+    /// Kopie des größeren Elements.
     pub fn max(&self, other: &Self) -> Self {
         if self > other {
             *self
@@ -40,7 +37,7 @@ impl Skalar {
         }
     }
 
-    /// Kopie des kleineren Elements
+    /// Kopie des kleineren Elements.
     pub fn min(&self, other: &Self) -> Self {
         if self < other {
             *self
@@ -53,12 +50,14 @@ impl Skalar {
 macro_rules! impl_from_mm {
     (mm:: $type:ident) => {
         impl mm::$type {
+            /// Konvertiere in ein [Skalar].
             pub const fn als_skalar(self) -> Skalar {
                 Skalar(self.0)
             }
         }
     };
 }
+
 impl_from_mm! {mm::Länge}
 impl_from_mm! {mm::Radius}
 impl_from_mm! {mm::Spurweite}
@@ -70,16 +69,19 @@ impl AddAssign<&Self> for Skalar {
         self.0 += rhs.0;
     }
 }
+
 impl AddAssign<&mut Self> for Skalar {
     fn add_assign(&mut self, rhs: &mut Self) {
         *self += &*rhs;
     }
 }
+
 impl AddAssign<Self> for Skalar {
     fn add_assign(&mut self, rhs: Self) {
         *self += &rhs;
     }
 }
+
 impl<T> Add<T> for Skalar
 where
     Skalar: AddAssign<T>,
@@ -91,13 +93,15 @@ where
         self
     }
 }
+
 // Monoid
 impl Skalar {
-    /// Additiv neutrales Element (0.)
+    /// Additiv neutrales Element `Skalar(0.)`.
     pub const fn additiv_neutral() -> Self {
         Skalar(0.)
     }
 }
+
 // inverses Element
 impl Neg for Skalar {
     type Output = Self;
@@ -107,21 +111,25 @@ impl Neg for Skalar {
         self
     }
 }
+
 impl SubAssign<Self> for Skalar {
     fn sub_assign(&mut self, rhs: Self) {
         *self += rhs.neg();
     }
 }
+
 impl SubAssign<&Self> for Skalar {
     fn sub_assign(&mut self, rhs: &Self) {
         *self -= rhs.clone();
     }
 }
+
 impl SubAssign<&mut Self> for Skalar {
     fn sub_assign(&mut self, rhs: &mut Self) {
         *self -= &*rhs;
     }
 }
+
 impl<T> Sub<T> for Skalar
 where
     Skalar: SubAssign<T>,
@@ -141,16 +149,19 @@ impl MulAssign<&Self> for Skalar {
         self.0 *= rhs.0;
     }
 }
+
 impl MulAssign<&mut Self> for Skalar {
     fn mul_assign(&mut self, rhs: &mut Self) {
         *self *= &*rhs;
     }
 }
+
 impl MulAssign<Self> for Skalar {
     fn mul_assign(&mut self, rhs: Self) {
         *self *= &rhs;
     }
 }
+
 impl<T> Mul<T> for Skalar
 where
     Skalar: MulAssign<T>,
@@ -162,29 +173,34 @@ where
         self
     }
 }
+
 // Monoid
 impl Skalar {
-    /// Multiplikativ neutrales Element (1.)
+    /// Multiplikativ neutrales Element `Skalar(1.)`.
     pub const fn multiplikativ_neutral() -> Self {
         Skalar(1.)
     }
 }
+
 // Inverses Element (via division)
 impl DivAssign<&Self> for Skalar {
     fn div_assign(&mut self, rhs: &Self) {
         *self *= Skalar(1. / rhs.0);
     }
 }
+
 impl DivAssign<&mut Self> for Skalar {
     fn div_assign(&mut self, rhs: &mut Self) {
         *self /= &*rhs;
     }
 }
+
 impl DivAssign<Self> for Skalar {
     fn div_assign(&mut self, rhs: Self) {
         *self /= &rhs;
     }
 }
+
 impl<T> Div<T> for Skalar
 where
     Skalar: DivAssign<T>,
