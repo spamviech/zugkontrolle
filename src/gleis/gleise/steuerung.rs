db19f5501be3cc54282dffc522a0f0134a0e07f0
spamviech <spamviech@web.de>
2022-04-20 12:33:53 +0200

    speicher Sender statt closure in Steuerung-Struktur

--- src/gleis/gleise/steuerung.rs
+++ src/gleis/gleise/steuerung.rs
@@ -26,10 +26,10 @@ use crate::{
 /// Steuerung eines Gleises.
 /// Mit dem Drop-Handler wird ein [Neuzeichen des Canvas](Cache::leeren) ausgelöst.
 #[derive(Clone)]
-pub struct Steuerung<T, F> {
+pub struct Steuerung<T, Nachricht> {
     steuerung: T,
     canvas: Arc<Mutex<Cache>>,
-    sende_nachricht: F,
+    sender: Sender<Nachricht>,
 }
 
 // Explizite Implementierung, um einen stack-overflow zu vermeiden.
@@ -38,7 +38,7 @@ impl<T: Debug, F> Debug for Steuerung<T, F> {
         f.debug_struct("Steuerung")
             .field("steuerung", &self.steuerung)
             .field("canvas", &"<Cache>")
-            .field("sende_nachricht", &"<funktion>")
+            .field("sender", &self.sender)
             .finish()
     }
 }
@@ -49,45 +49,49 @@ impl<T, F> AsRef<T> for Steuerung<T, F> {
     }
 }
 
-impl<T, F: FnMut()> AsMut<T> for Steuerung<T, F> {
+/// Eine asynchrone Aktion hat eine Änderung des Zustands bewirkt.
+#[derive(Debug, Clone, Copy)]
+pub struct AsyncAktualisieren;
+
+impl<T, Nachricht: From<AsyncAktualisieren>> AsMut<T> for Steuerung<T, Nachricht> {
     fn as_mut(&mut self) -> &mut T {
         self.canvas.lock().leeren();
-        (self.sende_nachricht)();
+        let _ = self.sender.send(AsyncAktualisieren.into());
         &mut self.steuerung
     }
 }
 
-impl<T, F> Steuerung<T, F> {
+impl<T, Nachricht> Steuerung<T, Nachricht> {
     /// Erstelle eine neue [Steuerung].
-    pub fn neu(steuerung: T, canvas: Arc<Mutex<Cache>>, sende_nachricht: F) -> Self {
-        Steuerung { steuerung, canvas, sende_nachricht }
+    pub fn neu(steuerung: T, canvas: Arc<Mutex<Cache>>, sender: Sender<Nachricht>) -> Self {
+        Steuerung { steuerung, canvas, sender }
     }
 
     /// Erzeuge eine neue [Steuerung], die nur einen Teil der Steuerung überwacht.
-    pub fn konvertiere<S>(self, f: impl FnOnce(T) -> S) -> Steuerung<S, F> {
-        let Steuerung { steuerung, canvas, sende_nachricht } = self;
-        Steuerung { steuerung: f(steuerung), canvas, sende_nachricht }
+    pub fn konvertiere<S>(self, f: impl FnOnce(T) -> S) -> Steuerung<S, Nachricht> {
+        let Steuerung { steuerung, canvas, sender } = self;
+        Steuerung { steuerung: f(steuerung), canvas, sender }
     }
 }
 
-impl<T, F> Steuerung<&Option<T>, F> {
+impl<T, Nachricht> Steuerung<&Option<T>, Nachricht> {
     /// Erhalte eine Referenz, falls ein Wert vorhanden ist.
     pub fn opt_as_ref(&self) -> Option<&T> {
         self.as_ref().as_ref()
     }
 
     /// Betrachte die [Steuerung] nur, wenn der enthaltene Wert [Some] ist.
-    pub fn nur_some(self) -> Option<Steuerung<T, F>> {
-        let Steuerung { steuerung, canvas, sende_nachricht } = self;
+    pub fn nur_some(self) -> Option<Steuerung<T, Nachricht>> {
+        let Steuerung { steuerung, canvas, sender } = self;
         if let Some(steuerung) = steuerung {
-            Some(Steuerung { steuerung, canvas, sende_nachricht })
+            Some(Steuerung { steuerung, canvas, sender })
         } else {
             None
         }
     }
 }
 
-impl<T, F: FnMut()> Steuerung<&mut Option<T>, F> {
+impl<T, Nachricht: From<AsyncAktualisieren>> Steuerung<&mut Option<T>, Nachricht> {
     /// Erhalte den Wert der zugehörigen Option-Referenz und hinterlasse [None].
     pub fn take(&mut self) -> Option<T> {
         self.as_mut().take()
@@ -105,17 +109,17 @@ impl<T, F: FnMut()> Steuerung<&mut Option<T>, F> {
     }
 }
 
-impl<'t, T, F> Steuerung<&'t mut Option<T>, F> {
+impl<'t, T, Nachricht> Steuerung<&'t mut Option<T>, Nachricht> {
     /// Erhalte eine Referenz, falls ein Wert vorhanden ist.
     pub fn opt_as_ref(&self) -> Option<&T> {
         self.as_ref().as_ref()
     }
 
     /// Betrachte die [Steuerung] nur, wenn der enthaltene Wert [Some] ist.
-    pub fn nur_some(self) -> Option<Steuerung<&'t mut T, F>> {
-        let Steuerung { steuerung, canvas, sende_nachricht } = self;
+    pub fn nur_some(self) -> Option<Steuerung<&'t mut T, Nachricht>> {
+        let Steuerung { steuerung, canvas, sender } = self;
         if let Some(steuerung) = steuerung {
-            Some(Steuerung { steuerung, canvas, sende_nachricht })
+            Some(Steuerung { steuerung, canvas, sender })
         } else {
             None
         }
@@ -141,23 +145,14 @@ pub trait MitSteuerung<'t> {
     ) -> Steuerung<&'t mut Self::Steuerung, F>;
 }
 
-/// Eine asynchrone Aktion hat eine Änderung des Zustands bewirkt.
-#[derive(Debug, Clone, Copy)]
-pub struct AsyncAktualisieren;
-
 impl<L: Leiter> Gleise<L> {
     #[zugkontrolle_macros::erstelle_daten_methoden]
     /// Erhalte die [Steuerung] für das spezifizierte Gleis.
-    pub(crate) fn erhalte_steuerung<
-        't,
-        T: 't + MitSteuerung<'t> + DatenAuswahl,
-        Nachricht: From<AsyncAktualisieren>,
-    >(
+    pub(crate) fn erhalte_steuerung<'t, T: 't + MitSteuerung<'t> + DatenAuswahl, Nachricht>(
         &'t self,
         gleis_id: &GleisId<T>,
         sender: Sender<Nachricht>,
-    ) -> Result<Steuerung<&'t <T as MitSteuerung<'t>>::Steuerung, impl FnMut()>, GleisIdFehler>
-    {
+    ) -> Result<Steuerung<&'t <T as MitSteuerung<'t>>::Steuerung, Nachricht>, GleisIdFehler> {
         let GleisId { rectangle, streckenabschnitt, phantom: _ } = gleis_id;
         let Gleise { zustand, canvas, .. } = self;
         let Gleis { definition, position: _ }: &Gleis<T> = &zustand
@@ -167,23 +162,16 @@ impl<L: Leiter> Gleise<L> {
             .next()
             .ok_or(GleisIdFehler::GleisEntfernt)?
             .data;
-        let sende_nachricht = || {
-            let _ = sender.send(AsyncAktualisieren.into());
-        };
-        Ok(definition.steuerung(canvas.clone(), sende_nachricht))
+        Ok(definition.steuerung(canvas.clone(), sender))
     }
 
     #[zugkontrolle_macros::erstelle_daten_methoden]
     /// Erhalte die [Steuerung] für das spezifizierte Gleis.
-    pub(crate) fn erhalte_steuerung_mut<
-        't,
-        T: 't + MitSteuerung<'t> + DatenAuswahl,
-        Nachricht: From<AsyncAktualisieren>,
-    >(
+    pub(crate) fn erhalte_steuerung_mut<'t, T: 't + MitSteuerung<'t> + DatenAuswahl, Nachricht>(
         &'t mut self,
         gleis_id: &GleisId<T>,
         sender: Sender<Nachricht>,
-    ) -> Result<Steuerung<&'t mut <T as MitSteuerung<'t>>::Steuerung, impl FnMut()>, GleisIdFehler>
+    ) -> Result<Steuerung<&'t mut <T as MitSteuerung<'t>>::Steuerung, Nachricht>, GleisIdFehler>
     {
         let GleisId { rectangle, streckenabschnitt, phantom: _ } = gleis_id;
         let Gleise { zustand, canvas, .. } = self;
@@ -194,10 +182,7 @@ impl<L: Leiter> Gleise<L> {
             .next()
             .ok_or(GleisIdFehler::GleisEntfernt)?
             .data;
-        let sende_nachricht = || {
-            let _ = sender.send(AsyncAktualisieren.into());
-        };
-        Ok(definition.steuerung_mut(canvas.clone(), sende_nachricht))
+        Ok(definition.steuerung_mut(canvas.clone(), sender))
     }
 }
 
