de83c8af68a0ee762b53e2e9b5fffdae104cde75
spamviech <spamviech@web.de>
2022-04-29 20:32:24 +0200

    Lizenz-Text wird in scrollable angezeigt, Button der aktuellen Lizenz ausgegraut

--- src/application/lizenzen.rs
+++ src/application/lizenzen.rs
@@ -27,7 +27,7 @@ use crate::application::{
 
 #[derive(Debug, Clone)]
 enum InterneNachricht {
-    Aktuell(fn() -> String),
+    Aktuell(&'static str, fn() -> String),
     Schließen,
 }
 
@@ -42,9 +42,10 @@ pub enum Nachricht {
 #[derive(Debug)]
 pub struct Zustand {
     lizenzen_und_button_states: BTreeMap<&'static str, (button::State, fn() -> String)>,
-    scrollable: scrollable::State,
+    scrollable_buttons: scrollable::State,
+    scrollable_text: scrollable::State,
     schließen: button::State,
-    aktuell: String,
+    aktuell: Option<(&'static str, String)>,
 }
 
 impl Zustand {
@@ -54,15 +55,15 @@ impl Zustand {
         let lizenzen_und_button_states = lizenzen
             .map(|(name, f)| {
                 if aktuell.is_none() {
-                    aktuell = Some(f());
+                    aktuell = Some((name, f()));
                 }
                 (name, (button::State::new(), f))
             })
             .collect();
-        let aktuell = aktuell.unwrap_or_else(String::new);
         Zustand {
             lizenzen_und_button_states,
-            scrollable: scrollable::State::new(),
+            scrollable_buttons: scrollable::State::new(),
+            scrollable_text: scrollable::State::new(),
             schließen: button::State::new(),
             aktuell,
         }
@@ -78,7 +79,7 @@ impl Zustand {
 /// Widget zur Anzeige der Lizenzen verwendeten Open-Source Bibliotheken.
 pub struct Lizenzen<'a, R: Renderer + container::Renderer> {
     container: Container<'a, InterneNachricht, R>,
-    aktuell: &'a mut String,
+    aktuell: &'a mut Option<(&'static str, String)>,
 }
 
 impl<R: Renderer + container::Renderer> Debug for Lizenzen<'_, R> {
@@ -105,29 +106,48 @@ where
         zustand: &'a mut Zustand,
         scrollable_style: impl Into<<R as scrollable::Renderer>::Style>,
     ) -> Self {
-        let Zustand { lizenzen_und_button_states, scrollable, schließen, aktuell } = zustand;
-        let mut scrollable = Scrollable::new(scrollable)
+        let Zustand {
+            lizenzen_und_button_states,
+            scrollable_buttons,
+            scrollable_text,
+            schließen,
+            aktuell,
+        } = zustand;
+        let mut buttons = Scrollable::new(scrollable_buttons)
             .width(Length::Shrink)
             .height(Length::Fill)
             .style(scrollable_style);
+        let (aktuell_name, aktuell_text) =
+            if let Some((name, text)) = aktuell { (Some(*name), Some(text)) } else { (None, None) };
         for (&name, (button_state, f)) in lizenzen_und_button_states {
-            scrollable = scrollable.push(
-                Button::new(button_state, Text::new(name)).on_press(InterneNachricht::Aktuell(*f)),
-            );
+            buttons = buttons.push({
+                let button = Button::new(button_state, Text::new(name));
+                if Some(name) == aktuell_name {
+                    button
+                } else {
+                    button.on_press(InterneNachricht::Aktuell(name, *f))
+                }
+            });
         }
         let column = Column::new()
-            .push(scrollable)
+            .push(buttons)
             .push(
                 Button::new(schließen, Text::new("Schließen"))
                     .on_press(InterneNachricht::Schließen),
             )
             .width(Length::Shrink)
             .height(Length::Fill);
+        let mut scrollable_aktuell =
+            Scrollable::new(scrollable_text).width(Length::Fill).height(Length::Fill);
+        if let Some(aktuell_text) = aktuell_text {
+            scrollable_aktuell = scrollable_aktuell
+                .push(Text::new(aktuell_text.as_str()).width(Length::Fill).height(Length::Fill))
+        }
         let container = Container::new(
             Row::new()
                 .push(column)
                 .push(Rule::vertical(1).style(TRENNLINIE))
-                .push(Text::new(aktuell.as_str()).width(Length::Fill).height(Length::Fill)),
+                .push(scrollable_aktuell),
         )
         .style(hintergrund::WEIß);
         Lizenzen { container, aktuell }
@@ -157,7 +177,7 @@ impl<'a, R: 'a + Renderer + container::Renderer> Widget<Nachricht, R> for Lizenz
         );
         for interne_nachricht in interne_nachrichten {
             match interne_nachricht {
-                InterneNachricht::Aktuell(f) => *self.aktuell = f(),
+                InterneNachricht::Aktuell(name, f) => *self.aktuell = Some((name, f())),
                 InterneNachricht::Schließen => nachrichten.push(Nachricht::Schließen),
             }
         }
@@ -177,7 +197,7 @@ impl<'a, R: 'a + Renderer + container::Renderer> From<Lizenzen<'a, R>>
 pub fn verwendete_lizenzen() -> impl Iterator<Item = (&'static str, fn() -> String)> {
     // FIXME verwende echte Lizenzen
     let f: fn() -> String = || {
-        String::from("Some long license text.\n\nTherefore, it needs multiple lines!\n\nNO WARRANTIES GIVEN, PROVIDED AS IS, ect.")
+        String::from("Some long license text.\n\nTherefore, it needs multiple lines!\n\nNO WARRANTIES GIVEN, PROVIDED AS IS, ect.\n\n\n\n\n\n\n\n\n\n\nSome text in the middle.\n\n\n\n\n\n\nAnother midway text.\n\n\n\n\n\n\n\nYet another debug line.\n\n\n\nHello from the deep.\n\n\n\n\nA final last line after a lot of vertical space.")
     };
     let g: fn() -> String = || {
         String::from("Ein andere Lizenz.\nAußerdem gibt es dabei sehr lange Texte, die ausreichen sollten um neben expliziten neuen Zeilen auch automatische Zeilenumbrüche überprüfen zu können.\n\nNO WARRANTIES GIVEN, PROVIDED AS IS, ect.")
