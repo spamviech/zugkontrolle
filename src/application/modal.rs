de658a66c7be300bc5198432fbfc9e3f75133050
spamviech <spamviech@web.de>
2022-08-12 18:53:27 +0200

    Fortschritte modal als pure widget

--- src/application/modal.rs
+++ src/application/modal.rs
@@ -1,6 +1,6 @@
 //! Überdecke ein Widget einem anderen Widget.
 
-use std::fmt::Debug;
+use std::fmt::{self, Debug, Formatter};
 
 use iced::{Rectangle, Size};
 use iced_native::{
@@ -11,7 +11,7 @@ use iced_native::{
     Clipboard, Event, Layout, Length, Point, Shell,
 };
 use iced_pure::{
-    overlay::{self, Overlay},
+    overlay,
     widget::{
         tree::{self, Tag, Tree},
         Container,
@@ -21,86 +21,86 @@ use iced_pure::{
 
 use crate::application::style::hintergrund::Hintergrund;
 
+/// Nachricht für Underlay und Overlay eines [Modals](Modal).
+pub enum Nachricht<Overlay, ElementNachricht> {
+    /// Nach außen durchgereichte Nachricht.
+    Underlay(ElementNachricht),
+    /// Ändere das angezeigte Overlay.
+    ZeigeOverlay(Overlay),
+    /// Verstecke das angezeigte Overlay.
+    VersteckeOverlay,
+}
+
 /// Zustand des [Modal]-Widgets.
 #[derive(Debug)]
-pub struct Zustand<Overlay> {
+struct Zustand<Overlay> {
     overlay: Option<Overlay>,
 }
 
 impl<Overlay> Zustand<Overlay> {
     /// Erstelle einen neuen Zustand für das [Modal]-Widget.
-    pub fn neu() -> Self {
+    fn neu() -> Self {
         Zustand { overlay: None }
     }
 
     // FIXME funktioniert stateless mit beeinflussen von außen?
+    // Nein, aber spezielle Nachrichten sind möglich
     /// Zeige ein Overlay über dem Widget.
     #[inline(always)]
-    pub fn zeige_modal(&mut self, overlay: Overlay) {
+    fn zeige_overlay(&mut self, overlay: Overlay) {
         self.overlay = Some(overlay);
     }
 
     /// Lösche das Overlay, so dass nur das originale Widget sichtbar ist.
     #[inline(always)]
-    pub fn verstecke_modal(&mut self) {
+    fn verstecke_overlay(&mut self) {
         self.overlay = None;
     }
 
     /// Das aktuell gezeigte Overlay.
     #[inline(always)]
-    pub fn overlay(&self) -> &Option<Overlay> {
+    fn overlay(&self) -> &Option<Overlay> {
         &self.overlay
     }
 
     /// Eine veränderliche Referenz auf das aktuelle Overlay.
     #[inline(always)]
-    pub fn overlay_mut(&mut self) -> &mut Option<Overlay> {
+    fn overlay_mut(&mut self) -> &mut Option<Overlay> {
         &mut self.overlay
     }
 }
 
 /// Ein Widget, dass ein Overlay vor einem anderen Widget anzeigen kann.
-pub struct Modal<'a, Overlay, Nachricht, R> {
-    zustand: &'a mut Zustand<Overlay>,
-    underlay: Element<'a, Nachricht, R>,
-    zeige_overlay: Box<dyn 'a + for<'t> Fn(&'t mut Overlay) -> Element<'t, Nachricht, R>>,
-    esc_nachricht: Option<Box<dyn 'a + Fn() -> Nachricht>>,
+pub struct Modal<'a, Overlay, ElementNachricht, R> {
+    underlay: Element<'a, Nachricht<Overlay, ElementNachricht>, R>,
+    zeige_overlay: &'a dyn Fn(&Overlay) -> Element<'_, Nachricht<Overlay, ElementNachricht>, R>,
+    schließe_bei_esc: bool,
 }
 
-impl<Overlay: Debug, Nachricht, R> Debug for Modal<'_, Overlay, Nachricht, R> {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+impl<Overlay, Nachricht, R> Debug for Modal<'_, Overlay, Nachricht, R> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
         f.debug_struct("Modal")
-            .field("zustand", &self.zustand)
             .field("underlay", &"<Element>")
             .field("zeige_overlay", &"<closure>")
-            .field("esc_nachricht", &self.esc_nachricht.as_ref().map(|_| "<closure>"))
+            .field("schließe_bei_esc", &self.schließe_bei_esc)
             .finish()
     }
 }
 
-impl<'a, Overlay, Nachricht, R> Modal<'a, Overlay, Nachricht, R> {
+impl<'a, Overlay, ElementNachricht, R> Modal<'a, Overlay, ElementNachricht, R> {
     /// Erstelle ein neues [Modal].
     pub fn neu(
-        zustand: &'a mut Zustand<Overlay>,
-        underlay: impl 'a + Into<Element<'a, Nachricht, R>>,
-        zeige_overlay: impl 'a + for<'t> Fn(&'t mut Overlay) -> Element<'t, Nachricht, R>,
+        underlay: impl 'a + Into<Element<'a, Nachricht<Overlay, ElementNachricht>, R>>,
+        zeige_overlay: &'a impl Fn(&Overlay) -> Element<'_, Nachricht<Overlay, ElementNachricht>, R>,
+        schließe_bei_esc: bool,
     ) -> Self {
-        Modal {
-            zustand,
-            underlay: underlay.into(),
-            zeige_overlay: Box::new(zeige_overlay),
-            esc_nachricht: None,
-        }
-    }
-
-    /// Erzeuge die gegebene Nachricht, wenn die Esc-Taste gedrückt wird.
-    pub fn on_esc(mut self, esc_nachricht: impl 'a + Fn() -> Nachricht) -> Self {
-        self.esc_nachricht = Some(Box::new(esc_nachricht));
-        self
+        Modal { underlay: underlay.into(), zeige_overlay, schließe_bei_esc }
     }
 }
 
-impl<Overlay, Nachricht, R: Renderer> Widget<Nachricht, R> for Modal<'_, Overlay, Nachricht, R> {
+impl<Overlay, ElementNachricht, R: Renderer> Widget<ElementNachricht, R>
+    for Modal<'_, Overlay, ElementNachricht, R>
+{
     fn width(&self) -> Length {
         self.underlay.as_widget().width()
     }
@@ -122,11 +122,11 @@ impl<Overlay, Nachricht, R: Renderer> Widget<Nachricht, R> for Modal<'_, Overlay
     }
 
     fn state(&self) -> tree::State {
-        todo!()
+        tree::State::new(Zustand::<Overlay>::neu())
     }
 
     fn tag(&self) -> Tag {
-        todo!()
+        Tag::of::<Zustand<Overlay>>()
     }
 
     fn draw(
@@ -153,13 +153,20 @@ impl<Overlay, Nachricht, R: Renderer> Widget<Nachricht, R> for Modal<'_, Overlay
         state: &'s mut Tree,
         layout: Layout<'_>,
         renderer: &R,
-    ) -> Option<overlay::Element<'s, Nachricht, R>> {
-        if let Some(overlay) = self.zustand.overlay_mut() {
+    ) -> Option<overlay::Element<'s, ElementNachricht, R>> {
+        let zustand: &Zustand<Overlay> = state.state.downcast_ref();
+        if let Some(overlay) = zustand.overlay() {
             let bounds = layout.bounds();
             let position = Point::new(bounds.x, bounds.y);
             Some(ModalOverlay::neu((self.zeige_overlay)(overlay)).overlay(position))
         } else {
-            self.underlay.as_widget().overlay(&mut state.children[0], layout, renderer)
+            self.underlay.as_widget().overlay(&mut state.children[0], layout, renderer).map(
+                |overlay| {
+                    let bounds = layout.bounds();
+                    let position = Point::new(bounds.x, bounds.y);
+                    OtherOverlay::neu(overlay).overlay(position)
+                },
+            )
         }
     }
 
@@ -188,28 +195,37 @@ impl<Overlay, Nachricht, R: Renderer> Widget<Nachricht, R> for Modal<'_, Overlay
         cursor_position: Point,
         renderer: &R,
         clipboard: &mut dyn Clipboard,
-        shell: &mut Shell<'_, Nachricht>,
+        shell: &mut Shell<'_, ElementNachricht>,
     ) -> event::Status {
-        if self.zustand.overlay.is_none() {
-            self.underlay.as_widget_mut().on_event(
+        let zustand: &mut Zustand<Overlay> = state.state.downcast_mut();
+        if zustand.overlay.is_none() {
+            let mut messages = Vec::new();
+            let mut inner_shell = Shell::new(&mut messages);
+            let status = self.underlay.as_widget_mut().on_event(
                 &mut state.children[0],
                 event,
                 layout,
                 cursor_position,
                 renderer,
                 clipboard,
-                shell,
-            )
+                &mut inner_shell,
+            );
+            if inner_shell.are_widgets_invalid() {
+                shell.invalidate_widgets()
+            } else if inner_shell.is_layout_invalid() {
+                shell.invalidate_layout()
+            }
+            for message in messages {
+                todo!()
+            }
+            status
         } else {
-            match (&self.esc_nachricht, event) {
-                (
-                    Some(esc_nachricht),
-                    Event::Keyboard(keyboard::Event::KeyPressed {
-                        key_code: KeyCode::Escape,
-                        modifiers: _,
-                    }),
-                ) => {
-                    shell.publish(esc_nachricht());
+            match event {
+                Event::Keyboard(keyboard::Event::KeyPressed {
+                    key_code: KeyCode::Escape,
+                    modifiers: _,
+                }) if self.schließe_bei_esc => {
+                    zustand.verstecke_overlay();
                     event::Status::Captured
                 },
                 _ => event::Status::Ignored,
@@ -229,10 +245,14 @@ where
     }
 }
 
-struct ModalOverlay<'a, Nachricht, R>(Element<'a, Nachricht, R>);
+struct ModalOverlay<'a, Overlay, ElementNachricht, R>(
+    Element<'a, Nachricht<Overlay, ElementNachricht>, R>,
+);
 
-impl<'a, Nachricht: 'a, R: 'a + Renderer> ModalOverlay<'a, Nachricht, R> {
-    fn neu(overlay: Element<'a, Nachricht, R>) -> Self {
+impl<'a, Overlay, ElementNachricht: 'a, R: 'a + Renderer>
+    ModalOverlay<'a, Overlay, ElementNachricht, R>
+{
+    fn neu(overlay: Element<'a, Nachricht<Overlay, ElementNachricht>, R>) -> Self {
         ModalOverlay(
             Container::new(overlay)
                 .width(Length::Fill)
@@ -245,14 +265,18 @@ impl<'a, Nachricht: 'a, R: 'a + Renderer> ModalOverlay<'a, Nachricht, R> {
     }
 }
 
-impl<'a, Nachricht: 'a, R: 'a + Renderer> ModalOverlay<'a, Nachricht, R> {
-    fn overlay(self, position: Point) -> overlay::Element<'a, Nachricht, R> {
+impl<'a, Overlay, ElementNachricht: 'a, R: 'a + Renderer>
+    ModalOverlay<'a, Overlay, ElementNachricht, R>
+{
+    fn overlay(self, position: Point) -> overlay::Element<'a, ElementNachricht, R> {
         overlay::Element::new(position, Box::new(self))
     }
 }
 
-impl<Nachricht, R: Renderer> Overlay<Nachricht, R> for ModalOverlay<'_, Nachricht, R> {
-    fn layout(&self, renderer: &R, bounds: Size, position: Point) -> iced_native::layout::Node {
+impl<Overlay, ElementNachricht, R: Renderer> overlay::Overlay<ElementNachricht, R>
+    for ModalOverlay<'_, Overlay, ElementNachricht, R>
+{
+    fn layout(&self, renderer: &R, bounds: Size, position: Point) -> layout::Node {
         let mut layout = self.0.as_widget().layout(renderer, &layout::Limits::new(bounds, bounds));
         layout.move_to(position);
         layout
@@ -271,18 +295,29 @@ impl<Nachricht, R: Renderer> Overlay<Nachricht, R> for ModalOverlay<'_, Nachrich
         cursor_position: Point,
         renderer: &R,
         clipboard: &mut dyn Clipboard,
-        shell: &mut Shell<'_, Nachricht>,
+        shell: &mut Shell<'_, ElementNachricht>,
     ) -> event::Status {
         let tree = Tree::new(self.0.as_widget());
-        self.0.as_widget_mut().on_event(
+        let mut messages = Vec::new();
+        let mut inner_shell = Shell::new(&mut messages);
+        let status = self.0.as_widget_mut().on_event(
             &mut tree,
             event,
             layout,
             cursor_position,
             renderer,
             clipboard,
-            shell,
-        )
+            &mut inner_shell,
+        );
+        if inner_shell.are_widgets_invalid() {
+            shell.invalidate_widgets()
+        } else if inner_shell.is_layout_invalid() {
+            shell.invalidate_layout()
+        }
+        for message in messages {
+            todo!()
+        }
+        status
     }
 
     fn mouse_interaction(
@@ -295,5 +330,73 @@ impl<Nachricht, R: Renderer> Overlay<Nachricht, R> for ModalOverlay<'_, Nachrich
         let widget = self.0.as_widget();
         let tree = Tree::new(widget);
         self.0.as_widget().mouse_interaction(&tree, layout, cursor_position, viewport, renderer)
+    }
+}
+
+struct OtherOverlay<'a, Overlay, ElementNachricht, R>(
+    overlay::Element<'a, Nachricht<Overlay, ElementNachricht>, R>,
+);
+
+impl<'a, Overlay, ElementNachricht: 'a, R: 'a + Renderer>
+    OtherOverlay<'a, Overlay, ElementNachricht, R>
+{
+    fn neu(overlay: overlay::Element<'a, Nachricht<Overlay, ElementNachricht>, R>) -> Self {
+        OtherOverlay(overlay)
+    }
+}
+
+impl<'a, Overlay, ElementNachricht: 'a, R: 'a + Renderer>
+    OtherOverlay<'a, Overlay, ElementNachricht, R>
+{
+    fn overlay(self, position: Point) -> overlay::Element<'a, ElementNachricht, R> {
+        overlay::Element::new(position, Box::new(self))
+    }
+}
+
+impl<Overlay, ElementNachricht, R: Renderer> overlay::Overlay<ElementNachricht, R>
+    for OtherOverlay<'_, Overlay, ElementNachricht, R>
+{
+    fn layout(&self, renderer: &R, bounds: Size, position: Point) -> layout::Node {
+        let mut layout = self.0.layout(renderer, bounds);
+        layout.move_to(position);
+        layout
+    }
+
+    fn draw(&self, renderer: &mut R, style: &Style, layout: Layout<'_>, cursor_position: Point) {
+        self.0.draw(renderer, style, layout, cursor_position)
+    }
+
+    fn on_event(
+        &mut self,
+        event: Event,
+        layout: Layout<'_>,
+        cursor_position: Point,
+        renderer: &R,
+        clipboard: &mut dyn Clipboard,
+        shell: &mut Shell<'_, ElementNachricht>,
+    ) -> event::Status {
+        let mut messages = Vec::new();
+        let mut inner_shell = Shell::new(&mut messages);
+        let status =
+            self.0.on_event(event, layout, cursor_position, renderer, clipboard, &mut inner_shell);
+        if inner_shell.are_widgets_invalid() {
+            shell.invalidate_widgets()
+        } else if inner_shell.is_layout_invalid() {
+            shell.invalidate_layout()
+        }
+        for message in messages {
+            todo!()
+        }
+        status
+    }
+
+    fn mouse_interaction(
+        &self,
+        layout: Layout<'_>,
+        cursor_position: Point,
+        viewport: &Rectangle,
+        renderer: &R,
+    ) -> mouse::Interaction {
+        self.0.mouse_interaction(layout, cursor_position, viewport, renderer)
     }
 }
