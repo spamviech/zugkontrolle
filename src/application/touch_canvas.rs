578dd1128533b62804c2616f8c74b3de4d00223a
spamviech <spamviech@web.de>
2022-08-19 21:21:20 +0200

    stelle api-gleichheit für touch_canvas wieder her

--- src/application/touch_canvas.rs
+++ src/application/touch_canvas.rs
@@ -14,35 +14,55 @@ use log::trace;
 use crate::application::macros::widget_newtype_methods;
 
 /// [iced::Canvas]-Wrapper mit Touch-Event.
-pub struct Canvas<'a, Message, R>(Element<'a, Message, R>);
+pub struct Canvas<'a, P, Message, R> {
+    element: Element<'a, Message, R>,
+    program: P,
+    width: Option<Length>,
+    height: Option<Length>,
+}
 
-impl<Message, R> Debug for Canvas<'_, Message, R> {
+impl<P: Debug, Message, R> Debug for Canvas<'_, P, Message, R> {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        f.debug_tuple("Canvas").field(&"<Element>").finish()
+        f.debug_struct("Canvas")
+            .field("element", &"<Element>")
+            .field("program", &self.program)
+            .field("width", &self.width)
+            .field("height", &self.height)
+            .finish()
     }
 }
 
-impl<Message, B: Backend> Canvas<'_, Message, Renderer<B>> {
+impl<P: Program<Message>, Message, B: Backend> Canvas<'_, P, Message, Renderer<B>> {
     /// Erstelle einen neuen [Canvas].
-    pub fn new<P: Program<Message>>(
-        program: P,
-        width: Option<Length>,
-        height: Option<Length>,
-    ) -> Self {
-        let mut canvas = iced::pure::widget::Canvas::new(program);
-        if let Some(width) = width {
-            canvas = canvas.width(width)
+    pub fn new(program: P) -> Self {
+        let mut canvas = iced::pure::widget::Canvas::new(&program);
+        Canvas { element: canvas.into(), program, width: None, height: None }
+    }
+
+    pub fn width(mut self, width: Length) -> Self {
+        let mut canvas = iced::pure::widget::Canvas::new(&self.program).width(width);
+        self.width = Some(width);
+        if let Some(height) = self.height {
+            canvas = canvas.height(height);
         }
-        if let Some(height) = height {
-            canvas = canvas.height(height)
+        self.element = canvas.into();
+        self
+    }
+
+    pub fn height(mut self, height: Length) -> Self {
+        let mut canvas = iced::pure::widget::Canvas::new(&self.program).height(height);
+        self.height = Some(height);
+        if let Some(width) = self.width {
+            canvas = canvas.height(width);
         }
-        Canvas(canvas.into())
+        self.element = canvas.into();
+        self
     }
 }
 
-impl<Message, R> Widget<Message, R> for Canvas<'_, Message, R> {
+impl<P, Message, R> Widget<Message, R> for Canvas<'_, P, Message, R> {
     widget_newtype_methods! {
-        0, R, Message
+        element, R, Message
         => [width, height, layout, draw, children, diff, mouse_interaction, overlay]
     }
 
@@ -80,7 +100,7 @@ impl<Message, R> Widget<Message, R> for Canvas<'_, Message, R> {
                 },
             }
         }
-        self.0.as_widget_mut().on_event(
+        self.element.as_widget_mut().on_event(
             &mut state.children[0],
             event,
             layout,
@@ -92,8 +112,8 @@ impl<Message, R> Widget<Message, R> for Canvas<'_, Message, R> {
     }
 }
 
-impl<'a, Message, R> From<Canvas<'a, Message, R>> for Element<'a, Message, R> {
-    fn from(canvas: Canvas<'a, Message, R>) -> Self {
+impl<'a, P, Message, R> From<Canvas<'a, P, Message, R>> for Element<'a, Message, R> {
+    fn from(canvas: Canvas<'a, P, Message, R>) -> Self {
         Element::new(canvas)
     }
 }
