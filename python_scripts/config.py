fc8da997375033908448badb06cf4832ff5c46f7
spamviech <spamviech@web.de>
2023-08-20 19:45:27 +0200

    verwende config.ini, falls sie existiert

--- python_scripts/config.py
+++ python_scripts/config.py
@@ -1,24 +1,14 @@
 #!/bin/python3
 
 import os.path
+import sys
 from configparser import ConfigParser
 from typing import Iterable
-
-# default values if the config.ini doesn't exist yet
-raspberry_address: str = "raspberrypi"
-raspberry_user: str = "pi"
 
 raspi32_target: str = "armv7-unknown-linux-gnueabihf"
 raspi64_target: str = "aarch64-unknown-linux-gnu"
 windows_target: str = "x86_64-pc-windows-gnu"
 linux_target: str = "x86_64-unknown-linux-gnu"
-
-targets: list[str] = [
-    raspi32_target,
-    raspi64_target,
-    windows_target,
-    linux_target,
-]
 
 section_raspi: str = "raspi"
 key_address = "address"
@@ -40,17 +30,17 @@ default_values: dict[str, dict[str, str]] = {
 }
 
 config_path: str = os.path.join(os.path.dirname(__file__), "config.ini")
-config_parser = ConfigParser(defaults=default_values)
+config_parser = ConfigParser()
+
+config_parser.read_dict(default_values)
 config_parser.read(config_path)
 
-
-def get_raspi_address() -> str:
-    return config_parser.get(section_raspi, key_address)
-
-
-def get_raspi_user() -> str:
-    return config_parser.get(section_raspi, key_user)
-
-
-def get_targets() -> Iterable[str]:
-    return map(lambda t: t[0], filter(lambda t: t[1], config_parser.items(section_targets)))
+raspberry_address: str = config_parser.get(section_raspi, key_address)
+raspberry_user: str = config_parser.get(section_raspi, key_user)
+targets: list[str] = []
+for target in config_parser.options(section_targets):
+    try:
+        if config_parser.getboolean(section_targets, target):
+            targets.append(target)
+    except ValueError as e:
+        print(f"Invalid value for target '{target}': {e}", file=sys.stderr)
