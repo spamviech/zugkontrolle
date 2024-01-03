d5bde38865ee8b43364c9c698a159b253f1a1c28
spamviech <spamviech@web.de>
2024-01-03 23:41:30 +0100

    Ignoriere unused_crate_dependencies in main.rs
    Es ist nur ein (extrem) shallow wrapper. Alle explizite nicht zu verwenden sorgt nur zu nerviger Synchronisations-Arbeit mit der Cargo.toml.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 //! Steuerung einer Model-Eisenbahn über einen Raspberry Pi.
 
+#![allow(unused_crate_dependencies)]
+
 use zugkontrolle::application::{ausführen_aus_env, Fehler};
 
 fn main() -> Result<(), Fehler> {
