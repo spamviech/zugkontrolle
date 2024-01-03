f137ca9829902f1abb29c4d38c7442d9e4829b63
spamviech <spamviech@web.de>
2024-01-03 11:28:25 +0100

    rebuild-flag für deploy-script

--- python_scripts/deploy.py
+++ python_scripts/deploy.py
@@ -1,11 +1,13 @@
 #!/bin/python3
 
 import os.path
+import sys
 
 from action import get_bin_path, build, send_to_raspi, check_docker_podman
 from util import is_raspi_target
 import config
 
+rebuild = ("-r" in sys.argv) or ("--rebuild" in sys.argv)
 checked_docker_podman_running = False
 
 # only deploy activated targets
@@ -13,7 +15,7 @@ for raspi_target in filter(is_raspi_target, config.targets):
     # build for raspi
     bin_path = get_bin_path(target=raspi_target)
     # re-build if the file doesn't exist
-    if not os.path.exists(bin_path):
+    if rebuild or (not os.path.exists(bin_path)):
         # check if docker/podman is running
         if not checked_docker_podman_running:
             checked_docker_podman_running = check_docker_podman()
