63015ab7e1786a05cdf4fe64af740b55a693b900
spamviech <spamviech@web.de>
2024-01-03 10:35:05 +0100

    deploy.py baut neu, falls die datei aktuell nicht existiert

--- python_scripts/deploy.py
+++ python_scripts/deploy.py
@@ -2,18 +2,22 @@
 
 import os.path
 
-from action import get_bin_path, send_to_raspi
-
+from action import get_bin_path, build, send_to_raspi, check_docker_podman
+from util import is_raspi_target
 import config
 
-# This assumes, prepare_release.py was executed before running this script
-# Hint: you can adjust the config.ini to only build the target binaries
+checked_docker_podman_running = False
 
 # only deploy activated targets
-for raspi_target in filter(lambda t: t in config.targets, [config.raspi32_target, config.raspi64_target]):
+for raspi_target in filter(is_raspi_target, config.targets):
     # build for raspi
     bin_path = get_bin_path(target=raspi_target)
-    # check if the file exists
-    assert(os.path.isfile(bin_path))
+    # re-build if the file doesn't exist
+    if not os.path.exists(bin_path):
+        # check if docker/podman is running
+        if not checked_docker_podman_running:
+            checked_docker_podman_running = check_docker_podman()
+        # build the zugkontrolle binary
+        build(target=raspi_target)
     # automatically transfer to raspi using scp
     send_to_raspi(bin_path, config.raspberry_user, config.raspberry_address)
