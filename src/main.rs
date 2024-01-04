94625403a2547a5825bfb5a1b074c72e7ffb8578
spamviech <spamviech@web.de>
2024-01-05 00:00:08 +0100

    erlaube multiple_crate_versions auch in der main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 //! Steuerung einer Model-Eisenbahn über einen Raspberry Pi.
 
+// Werden in der lib verwendet. Alles explizit nicht zu benutzen führt nur zu Duplikation mit der `Cargo.toml`.
 #![allow(unused_crate_dependencies)]
+// Zu viele/große dependencies, um das wirklich zu vermeiden.
+#![allow(clippy::multiple_crate_versions)]
 
 use zugkontrolle::application::{ausführen_aus_env, Fehler};
 
