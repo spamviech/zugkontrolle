7fbf732af6ab2e7b493ac3a529f98702bc4d33c0
spamviech <spamviech@web.de>
2024-04-13 23:15:14 +0200

    neues nototools-submodule

--- python_scripts/convert_bootstrap_font.py
+++ python_scripts/convert_bootstrap_font.py
@@ -4,6 +4,7 @@ import os.path
 
 import util
 from woff2otf import woff2otf
+from nototools.nototools.subset import subset_font
 
 repo_dir: str = util.get_repo_root()
 font_dir: str = os.path.join(repo_dir, "fonts")
@@ -12,4 +13,3 @@ woff_path: str = os.path.join(woff_dir, "bootstrap-icons.woff")
 otf_path: str = os.path.join(font_dir, "bootstrap-icons.otf")
 
 woff2otf.convert(woff_path, otf_path)
-
